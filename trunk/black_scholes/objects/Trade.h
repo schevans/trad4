6cc28156a35e02b47dfa8bc8ae8de6a1cf982ca9
steve <steve@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2007-06-12 17:34:05 +0000

    Got working efficently

--- trunk/black_scholes/objects/Trade.h
+++ trunk/black_scholes/objects/Trade.h
@@ -28,8 +28,14 @@ private:
     double _ln_SoK;
     double _rT;
     double _v2T_2;
-    double _v_rtT;
+    double _v_RtT;
     double _KerT;
+
+    double _SoN_pd1;
+    double _SoN_md1;
+    double _KerTN_pd2;
+    double _KerTN_md2;
+
 };
 
 #endif
