11c3752060efbbb7bbbc52dd1416838551529a2c
steve <steve@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2007-06-23 18:14:15 +0000

    Tidied

--- trunk/black_scholes/objects/OptionVec.cpp
+++ trunk/black_scholes/objects/OptionVec.cpp
@@ -9,19 +9,16 @@ using namespace std;
 bool OptionVec::Calculate()
 {
     cout << "OptionVec::Calculate()" << endl;
-DBG
-DBG
+
     vector<Object*>::iterator iter;
     for( iter = _elements.begin() ; iter < _elements.end() ; iter++ )
     {
         (*iter)->Calculate();
-DBG
         (*iter)->Notify();
-DBG
     }
-DBG
+
     Notify();
-DBG
+
     return true;
 }
 
