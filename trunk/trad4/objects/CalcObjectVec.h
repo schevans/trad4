bd0cded0197411914844c0db8dee483bc56754f9
steve <steve@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2007-07-03 12:11:52 +0000

    Fresh checkout test - adding

---
+++ trunk/trad4/objects/CalcObjectVec.h
@@ -0,0 +1,40 @@
+
+// Copyright Steve Evans 2007
+// steve@topaz.myzen.co.uk
+
+#ifndef __CALC_OBJECT_VEC_H__
+#define __CALC_OBJECT_VEC_H__
+
+#include <string>
+#include <vector>
+
+#include "CalcObject.h"
+#include "trad4.h"
+
+
+class CalcObjectVec : public CalcObject {
+
+public:
+
+    virtual ~CalcObjectVec() {}
+
+    virtual void Run();
+    virtual bool Stop();
+
+    virtual bool AttachToSubscriptions() = 0;
+    virtual bool CheckSubscriptions() = 0;
+    virtual bool Save() = 0;
+    virtual bool Load() = 0;
+    virtual bool NeedRefresh() = 0;
+    virtual bool Calculate() = 0;
+    virtual int Type() = 0;
+
+protected:
+
+    std::vector<int> _element_ids;
+    std::vector<Object*> _elements;
+     
+
+};
+
+#endif
