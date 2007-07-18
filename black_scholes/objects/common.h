ceaea7db337761510ae28e6f09e20663bb689efc
steve <steve@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2007-07-18 13:24:00 +0000

    Adding licence

--- black_scholes/objects/common.h
+++ black_scholes/objects/common.h
@@ -1,3 +1,7 @@
+// Copyright (c) Steve Evans 2007
+// steve@topaz.myzen.co.uk
+// This code is licenced under the BSD licence. For details see $INSTANCE_ROOT/LICENCE
+
 
 #ifndef __common_h__
 #define __common_h__
