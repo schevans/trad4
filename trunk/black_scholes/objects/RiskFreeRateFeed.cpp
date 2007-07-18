f879aaa5258954ba2ba87f6bc19e2571f5ba9ed7
steve <steve@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2007-07-18 13:24:00 +0000

    Adding licence

--- trunk/black_scholes/objects/RiskFreeRateFeed.cpp
+++ trunk/black_scholes/objects/RiskFreeRateFeed.cpp
@@ -1,3 +1,8 @@
+
+// Copyright (c) Steve Evans 2007
+// steve@topaz.myzen.co.uk
+// This code is licenced under the BSD licence. For details see $INSTANCE_ROOT/LICENCE
+
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <iostream>
