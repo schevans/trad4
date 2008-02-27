be40ecec3f6941afd1c7526053a8364fd56b728e
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-02-27 19:46:18 +0000

    Split up into mult files.

---
+++ trad4/trad5/discount_rate.c
@@ -0,0 +1,45 @@
+// Copyright (c) Steve Evans 2008
+// steve@topaz.myzen.co.uk
+//
+
+#include <iostream>
+#include <math.h>
+
+#include "trad5.h"
+#include "common.h"
+#include "discount_rate.h"
+#include "interest_rate_feed.h"
+
+using namespace std;
+
+extern void* obj_loc[NUM_OBJECTS+1];
+
+extern void set_timestamp( int );
+
+void* calculate_discount_rate( void* id )
+{
+    // Hack to get this working. Otherwise lifted from trad_bond_risk.
+    void* _pub = obj_loc[(int)id];
+    interest_rate_feed* _sub_interest_rate_feed = (interest_rate_feed*)obj_loc[((discount_rate*)_pub)->interest_rate_feed];
+
+    cout << "DiscountRate::Calculate()" << endl;
+
+    for ( int i = 0 ; i < DATE_RANGE_LEN ; i++ )
+    {
+        //cout << "Libor: " << _sub_interest_rate_feed->rate_interpol[i] << ", Disco: " << exp( -_sub_interest_rate_feed->rate_interpol[i] * (( i / YEAR_BASIS)/ YEAR_BASIS ) ) << " Year frac: " << ( i/ YEAR_BASIS ) << endl;
+
+        ((discount_rate*)_pub)->rate[i] = exp( -_sub_interest_rate_feed->rate_interpol[i] * (( i / YEAR_BASIS)/ YEAR_BASIS ) );
+
+        ((discount_rate*)_pub)->rate_01[i] = exp( -(_sub_interest_rate_feed->rate_interpol[i] -0.0001) * (( i / YEAR_BASIS)/ YEAR_BASIS ) );
+    }
+
+    set_timestamp((int)id);
+}
+
+// More-or-less lifted from DiscountRateBase::NeedRefresh()
+bool discount_rate_need_refresh( int id )
+{
+    // This will be generated - doesn't matter if it's illegeble.
+    return ( ((object_header*)obj_loc[id])->status == STOPPED ) && ( *(int*)obj_loc[id] < *(int*)obj_loc[((discount_rate*)obj_loc[id])->interest_rate_feed] );
+}
+
