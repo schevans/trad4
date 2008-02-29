9167f9f2c3871aef762149ade187ba1a09eba66b
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-02-29 14:41:23 +0000

    Trad4v1 is dead. Long live trad4v2.

--- trunk/trad4/bin/kill_all.sh
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Steve Evans 2007
-# steve@topaz.myzen.co.uk
-# This code is licenced under the LGPL. For details see $TRAD4_ROOT/LICENCE
-
-
-for i in `ps -ef | grep steve | grep object | grep -v object_locator | grep -v object_viewer | grep " 1 " | awk '{print $2}'`; do kill -1 $i; done
-
--- trunk/trad4/objects/trad4.h
+++ trunk/trad4/objects/trad4.h
@@ -1,7 +1,5 @@
-
-// Copyright (c) Steve Evans 2007
+// Copyright (c) Steve Evans 2008
 // steve@topaz.myzen.co.uk
-// This code is licenced under the LGPL. For details see $TRAD4_ROOT/LICENCE
 
 
 #ifndef __TRAD4_H__
@@ -11,44 +9,30 @@
 
 #define MAX_OBJECTS 20000
 
-#define MAX_OB_FILE_LEN 100
-#define MAX_OB_FILE_NAME 20
-#define MAX_OBJECT_TYPES_FILE_LEN 120
-
-#define OBJ_LOC_SHMID_LEN 32
+#define NUM_OBJECTS 3
 
 #define OBJECT_NAME_LEN 32
-
-#define MONITOR_SLEEP_TIME 10
 
 enum log_level {
     NONE,
 };
 
 enum object_status {
-    UNKNOWN,
     STOPPED,
-    STARTING,
     RUNNING,
-    BLOCKED,
-    FAILED,
-    STALE,
-    MANAGED
+    FAILED
 };
 
 typedef struct {
-    int shmid[MAX_OBJECTS];
-} obj_loc;
-
-typedef struct {
+    // Header
     time_t last_published;
     object_status status;
-    int pid;
+    void* (*calculator_fpointer)(void*);
+    bool (*need_refresh_fpointer)(int);
     int type;
     char name[OBJECT_NAME_LEN];
     int sleep_time;
 } object_header;
-
 
 #define DBG cout << __FILE__ << ": " << __LINE__ << endl;
 
