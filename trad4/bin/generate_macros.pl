2223877d7823385393dca642d2751d0ef8b01e17
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-03-26 20:34:52 +0000

    Precursor to a reorganisation of the calculate accessors. Incomplete.

---
+++ trad4/bin/generate_macros.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/perl
+
+use warnings;
+use Data::Dumper;
+
+if ( !$ENV{TRAD4_ROOT} ) {
+
+    print "TRAD4_ROOT not set. Exiting\n";
+    exit 1;
+}
+
+sub load_defs($);
+sub print_accessors($$);
+
+my %object_hash;
+
+open TYPES_FILE, "$ENV{INSTANCE_ROOT}/defs/object_types.t4s" or die "Can't open $ENV{INSTANCE_ROOT}/defs/object_types.t4s for reading";
+
+while ( $line = <TYPES_FILE> ) {
+
+    chomp $line;
+
+    if ( !$line or $line =~ /#/ ) {
+        next;
+    }
+
+    ( $num, $tier, $type ) = split /,/, $line;
+
+    load_defs( $type );
+}
+
+close TYPES_FILE;
+
+open FILE, ">bs_delta_macros.h";
+
+
+print_accessors( "bs_delta", FILE );
+
+close FILE;
+
+print Dumper( %object_hash );
+
+sub print_accessors($$) {
+    my $name = shift;
+    my $FILEHANDLE = shift;
+
+
+        foreach $var ( keys %{$object_hash{$name}{pub}} ) {
+
+            print $FILEHANDLE "#define $name"."_$var ($name*)obj_loc[id]->$var\n"; 
+
+        }
+
+        foreach $var ( keys %{$object_hash{$name}{sub}} ) {
+
+            foreach $var2 ( keys %{$object_hash{$var}{pub}} ) {
+
+                print $FILEHANDLE "#define $var"."_$var2 ($var*)obj_loc[ (($name*)obj_loc[id])->$var]->$var2\n"; 
+            }
+
+            foreach $var2 ( keys %{$object_hash{$var}{static}} ) {
+
+                print $FILEHANDLE "#define $var"."_$var2 (($var*)obj_loc[(($name*)obj_loc[id])->$var])->$var2\n"; 
+            }
+        }
+
+}
+
+sub load_defs($) {
+    my $name = shift;
+
+    my $file = $ENV{INSTANCE_ROOT}."/defs/$name.t4";
+
+    open FILE, "$file" or die "Could not open $file for reading";
+
+
+    my $line;
+    my $section;
+
+    my ( $type, $var );
+
+    while ( $line = <FILE> ) {
+
+        chomp $line;
+
+        if ( !$line or $line =~ /#/ ) {
+            next;
+        }
+
+        $line =~ s/^ *//;
+
+        if ( $line =~ /sub|pub|static/ ) {
+
+            $section = $line;
+            next;
+        }
+
+        ( $type, $var ) = split / /, $line;
+
+        $object_hash{$name}{$section}{$var} = $type;
+    }
+
+    close FILE;
+
+}
+
