8056d10cdbd2995a9ad32edc2fd0eecf219f8ad0
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-03-27 20:42:55 +0000

    Changed to use the new dependant-independent signatures.

--- black_scholes/objects/risk_free_rate_feed.c
+++ black_scholes/objects/risk_free_rate_feed.c
@@ -1,7 +1,7 @@
 
 #include "risk_free_rate_feed_wrapper.c"
 
-void* calculate_risk_free_rate_feed( risk_free_rate_feed* pub_risk_free_rate_feed )
+void* calculate_risk_free_rate_feed( int id )
 {
 
 }
--- black_scholes/objects/stock_feed.c
+++ black_scholes/objects/stock_feed.c
@@ -1,7 +1,7 @@
 
 #include "stock_feed_wrapper.c"
 
-void* calculate_stock_feed( stock_feed* pub_stock_feed )
+void* calculate_stock_feed( int id )
 {
 
 }
