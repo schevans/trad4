9e480cf923e76459f5634bfce3e4128f18ae161d
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-03 19:17:24 +0000

    Added stock_trade & rate_trade.

--- black_scholes/objects/rate_trade.c
+++ black_scholes/objects/rate_trade.c
@@ -1,8 +1,19 @@
 
+#include <iostream>
+#include <math.h>
+
 #include "rate_trade_wrapper.c"
 
-void* calculate_rate_trade( rate_trade* pub_rate_trade , option_feed* sub_option_feed , risk_free_rate_feed* sub_risk_free_rate_feed )
-{
+using namespace std;
 
+void* calculate_rate_trade( int id )
+{
+    DEBUG( "calculate_rate_trade( " << id << ")" )
+
+    rate_trade_rT = risk_free_rate_feed_r * option_feed_T;
+
+    rate_trade_KerT = option_feed_T * exp( - rate_trade_rT );
+
+    rate_trade_rKerT = 0.0; //TODO 
 }
 
