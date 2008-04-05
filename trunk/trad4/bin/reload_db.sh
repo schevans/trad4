f7941315f03dca98d75aa94a72e089579026cc8d
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-05 19:45:47 +0000

    Added validation & reload_db.sh

---
+++ trunk/trad4/bin/reload_db.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+
+if [ $# -ne 1 ]
+then
+    echo "reload_db.sh <data_dir>"
+    exit 0
+fi
+
+
+echo "delete from object" | mysql -u root $INSTANCE
+
+DATA_DIR=$1
+
+cd $INSTANCE_ROOT/data/$DATA_DIR
+
+for i in `ls *.sql`
+do
+    $TRAD4_ROOT/bin/runsql $i
+
+    if [ $? != 0 ]
+    then
+        echo "Error encountered when loading $i. Exiting"
+        exit 0
+    fi
+done
+
+
--- trunk/trad4/bin/send_reload.sh
+++ trunk/trad4/bin/send_reload.sh
@@ -2,5 +2,12 @@
 
 PID=`ps -ef | grep $INSTANCE | grep -v grep | grep -v mysql | awk '{print $2}'`
 
-kill -10 $PID
+if [ $PID ]
+then
+    kill -10 $PID
+else
+    echo "$INSTANCE not found in ps. Are you sure $INSTANCE is running?"
+    exit 0
+fi
+
 
