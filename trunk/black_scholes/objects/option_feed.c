dba9bdc251f2d99bf3e3d742e2d75faede7fd292
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-05 19:54:26 +0000

    Further tinkering.

--- trunk/black_scholes/objects/option_feed.c
+++ trunk/black_scholes/objects/option_feed.c
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <math.h>
 
 #include "option_feed_wrapper.c"
 
@@ -8,5 +9,7 @@ using namespace std;
 void* calculate_option_feed( int id )
 {
     DEBUG( "calculate_option_feed( " << id << ")" )
+
+    option_feed_RrT = sqrt( option_feed_T );
 }
 
