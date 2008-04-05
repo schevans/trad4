0fbdc8b0b734a1003037f8e81e5a4fc64dd82571
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-05 19:43:41 +0000

    Still fleshing out model.

--- black_scholes/objects/stock_trade.c
+++ black_scholes/objects/stock_trade.c
@@ -12,7 +12,8 @@ void* calculate_stock_trade( int id )
 
     stock_trade_ln_SK = log( stock_feed_S / option_feed_K );
 
-    stock_trade_v2T_2 = stock_feed_v * sqrt( option_feed_T );
+    stock_trade_vvT_2 = ( stock_feed_vv * option_feed_T ) / 2.0;
 
+    stock_trade_vRtT = stock_feed_v * sqrt( option_feed_T );
 }
 
