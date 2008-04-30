f1f0afc673288660d3d874261b5f89ddfd78bcaf
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-30 18:32:08 +0000

    Cleaned-up.

--- trad4/PreComp/Sql.pm
+++ trad4/PreComp/Sql.pm
@@ -14,46 +14,23 @@ sub Generate($) {
 
     #print_licence_header( $FHD );
 
-print Dumper( $obj_hash );
-
-    my $tuple;
-    my ( $column, $type );
-
     print $FHD "create table $name (\n";
     print $FHD "    id int";
 
     foreach $key ( keys %{$obj_hash->{data}->{static}} ) {
-
-print "Key: $key\n";
 
         print $FHD ",\n    $key ".PreComp::Utilities::Type2Sql($obj_hash->{data}->{static}->{$key});
     }
 
     foreach $key ( keys %{$obj_hash->{data}->{feed_in}} ) {
 
-print "Jey: $key\n";
-
         print $FHD ",\n    $key ".PreComp::Utilities::Type2Sql($obj_hash->{data}->{static}->{$key});
     }
 
     foreach $key ( keys %{$obj_hash->{data}->{sub}} ) {
 
-print "Tey: $key\n";
-print "Stuff: $obj_hash->{data}->{sub}->{$key}\n";
-
         print $FHD ",\n    $key ".PreComp::Utilities::Type2Sql($obj_hash->{data}->{sub}->{$key});
     }
-#    foreach $tuple ( @static, @feed_in ) {
-#
-#        ( $type, $column ) = split / /, $tuple;
-#
-
-#    foreach $tuple ( @sub ) {
-#
-#        ( $type, $var ) = split / /, $tuple;
-#
-#        print $FHD ",\n    $var int";
-#    }
 
     print $FHD "\n";
     print $FHD ")\n";
