668d24010680b107357cf3a2cfde5ff6cc674c83
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-30 18:30:04 +0000

    Moved macros into PreComp and cleaned them up.

---
+++ trad4/PreComp/Macros.pm
@@ -0,0 +1,62 @@
+
+package PreComp::Macros;
+
+use PreComp::Utilities;
+use PreComp::Constants;
+use Data::Dumper;
+
+sub Generate($) {
+    my $obj_hash = shift;
+
+    foreach $name ( keys %{$obj_hash} ) {
+
+        my $FHD = PreComp::Utilities::OpenFile( PreComp::Constants::GenObjRoot().$name."_macros.h" );
+
+
+        #print_licence_header( $FHD );
+             
+                                              
+        print $FHD "\n";
+        print $FHD "#ifndef __$name"."_marcos_h__\n";
+        print $FHD "#define __$name"."_marcos_h__\n";
+        print $FHD "\n";
+
+        print $FHD "// $name macros\n";
+
+        foreach $var ( keys %{$obj_hash->{$name}->{data}->{pub}} ) {
+
+            print $FHD "#define $name"."_$var (($name*)obj_loc[id])->$var\n";
+
+        }
+
+        foreach $var ( keys %{$obj_hash->{$name}->{data}->{static}} ) {
+
+            print $FHD "#define $name"."_$var (($name*)obj_loc[id])->$var\n";
+
+        }
+
+
+        foreach $var ( keys %{$obj_hash->{$name}->{data}->{sub}} ) {
+
+            print $FHD "\n";
+            print $FHD "// sub $var macros\n";
+
+            foreach $var2 ( keys %{$obj_hash->{$var}->{data}->{pub}} ) {
+
+                print $FHD "#define $var"."_$var2 (($var*)obj_loc[(($name*)obj_loc[id])->$var])->$var2\n";
+            }
+
+            foreach $var2 ( keys %{$obj_hash->{$var}->{data}->{static}} ) {
+
+                print $FHD "#define $var"."_$var2 (($var*)obj_loc[(($name*)obj_loc[id])->$var])->$var2\n";
+            }
+        }
+
+        print $FHD "#endif\n";
+
+        PreComp::Utilities::CloseFile();
+    }
+}
+
+1;
+
