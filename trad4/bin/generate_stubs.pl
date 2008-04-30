c4587e2bf7a6fc9bac2d7d28a3cb791cdf81fc86
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-04-30 18:27:07 +0000

    Moved table gen into PreComp.

--- trad4/bin/generate_stubs.pl
+++ trad4/bin/generate_stubs.pl
@@ -97,7 +97,7 @@ load_defs( "$defs_root/$name.t4" );
 
 validate();
 #generate_h();
-generate_table();
+#generate_table();
 #generate_c_wrapper();
 #generate_loader();
 generate_dummy_data();
@@ -111,11 +111,13 @@ if ( ! -f "$obj_root/$c_filename" ) {
 use PreComp::Header;
 use PreComp::Wrapper;
 use PreComp::Utilities;
+use PreComp::Sql;
 
 my $master_hash = PreComp::Utilities::LoadDefs();
 
 PreComp::Header::Generate( $master_hash->{$name} );
 PreComp::Wrapper::Generate( $master_hash->{$name} );
+PreComp::Sql::Generate( $master_hash->{$name} );
 
 
 #############################################################
