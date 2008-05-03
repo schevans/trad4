102e6bad9b1eaf26edea6cee0560d9e2055f1320
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-05-03 10:14:24 +0000

    Adding extra_loader. More feed frigging..

--- bond_risk/objects/interest_rate_feed.c
+++ bond_risk/objects/interest_rate_feed.c
@@ -14,79 +14,33 @@ void calculate_interest_rate_feed( obj_loc_t obj_loc, int id )
     cout << "calculate_interest_rate_feed()" << endl;
 
 }
-/*
-void load_interest_rate_feeds( MYSQL* mysql )
+
+void extra_loader( obj_loc_t obj_loc, int id, MYSQL& mysql )
 {
-    //cout << "load_interest_rate_feeds()" << endl;
+    cout << "extra_loader()" << endl;
 
     MYSQL_RES *result;
     MYSQL_ROW row;
 
     ostringstream dbstream;
-    dbstream << "select id, name from object where type=1";
+    dbstream << "select asof, rate from interest_rate_feed_data where id=" << id;
 
-DBG
-    if(mysql_query(mysql, dbstream.str().c_str()) != 0) {
-        cout << __LINE__ << ": " << mysql_error(mysql) << endl;
+    if(mysql_query(&mysql, dbstream.str().c_str()) != 0) {
+        cout << __LINE__ << ": " << mysql_error(&mysql) << endl;
     }
 
-DBG
-    result = mysql_use_result(mysql);
+    result = mysql_use_result(&mysql);
 
-DBG
-    int id;
-    std::string name;
-    void* tmp;
-DBG
-   std::vector<int> interest_rates;
+    int counter(0);
 
     while (( row = mysql_fetch_row(result) )) {
 
-DBG
-       id = atoi(row[0]);
-       name = row[2];
+        interest_rate_feed_asof[counter]  = atoi(row[0]);
+        interest_rate_feed_rate[counter]  = atof(row[1]);
 
-        obj_loc[id] = new interest_rate_feed;
-        ((interest_rate_feed*)obj_loc[id])->last_published = 0;
-DBG
-        ((interest_rate_feed*)obj_loc[id])->status = STOPPED;
-        ((interest_rate_feed*)obj_loc[id])->calculator_fpointer = &calculate_interest_rate_feed_wrapper;
-        ((interest_rate_feed*)obj_loc[id])->need_refresh_fpointer = &interest_rate_feed_need_refresh;
-        ((interest_rate_feed*)obj_loc[id])->type = 1;
-DBG
-    //    ((interest_rate_feed*)obj_loc[id])->name = 0;
-
-
-        ((interest_rate_feed*)obj_loc[id])->shmid = create_shmem( &tmp, sizeof( interest_rate_feed_sub ) );
-DBG
-        ((interest_rate_feed*)obj_loc[id])->sub = (interest_rate_feed_sub*)tmp;
-//        (((interest_rate_feed*)obj_loc[id])->sub)->sub = 0;
-DBG
-
-        (((interest_rate_feed*)obj_loc[id])->sub)->last_published = 0;
-DBG
-
-        interest_rates.push_back( id );
-
+        counter++;
     }
 
     mysql_free_result(result);
 
-    fstream save_file("/tmp/interest_rate_feed.txt", ios::out);
-
-    vector<int>::iterator iter;
-
-    for( iter = interest_rates.begin() ; iter < interest_rates.end() ; iter++ )
-    {
-        save_file << *iter << "," << ((interest_rate_feed*)obj_loc[*iter])->shmid << endl;
-
-        for ( int i=0 ; i < INTEREST_RATE_LEN - 1 ; i++ )
-        {    
-            ((interest_rate_feed*)obj_loc[(int)id])->asof[i] = 0;
-            ((interest_rate_feed*)obj_loc[(int)id])->rate[i] = 0;
-        }
-    }
-    
-    save_file.close();
 }
-*/
