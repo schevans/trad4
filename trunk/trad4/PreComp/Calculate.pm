41cdcceea08e332e3d50c3bab41640851438f8fa
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-05-03 11:32:22 +0000

    Moved calcuate to PreComp.

---
+++ trunk/trad4/PreComp/Calculate.pm
@@ -0,0 +1,55 @@
+
+package PreComp::Calculate;
+
+use PreComp::Utilities;
+use PreComp::Constants;
+use Data::Dumper;
+
+sub Generate($) {
+    my $obj_hash = shift;
+
+    my $name = $obj_hash->{name};
+
+    my @header = PreComp::Constants::CommomHeader();
+
+    my $file = PreComp::Constants::ObjRoot()."$name.c";
+
+print "$file\n";
+
+    if ( -f $file ) {
+print "Returning\n";
+       return;
+    } 
+
+print "NOT Returning\n";
+    my $FHD = PreComp::Utilities::OpenFile( PreComp::Constants::ObjRoot()."$name.c" );
+
+    #print_licence_header( $FHD );
+
+    print $FHD "\n";
+    print $FHD "#include <iostream>\n";
+    print $FHD "\n";
+    print $FHD "#include \"$name"."_wrapper.c\"\n";
+    print $FHD "\n";
+    print $FHD "using namespace std;\n";
+    print $FHD "\n";
+    print $FHD "void calculate_$name( obj_loc_t obj_loc, int id )\n";
+    print $FHD "{\n";
+    print $FHD "    DEBUG( \"calculate_$name( \" << id << \")\" )\n";
+    print $FHD "}\n";
+    print $FHD "\n";
+
+    if ( %{$obj_hash->{data}->{static_vec}} ) {
+
+        print $FHD "\n";
+        print $FHD "void extra_loader( obj_loc_t obj_loc, int id, MYSQL& mysql )\n";
+        print $FHD "{\n";
+        print $FHD "    cout << \"extra_loader()\" << endl;\n";
+        print $FHD "\n";
+        print $FHD "}\n";
+    }
+
+    PreComp::Utilities::CloseFile();
+}
+
+1;
