3f1d7c39d8bc7e7e7f9c29d4c9a3915d2f2299e4
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-05-19 11:59:08 +0000

    generate_stubs.pl is dead! Long live precomp.pl!

--- trad4/PreComp/Sql.pm
+++ trad4/PreComp/Sql.pm
@@ -5,7 +5,57 @@ use PreComp::Utilities;
 use PreComp::Constants;
 use Data::Dumper;
 
-sub Generate($) {
+sub generate_table($);
+sub generate_dummy_data($$);
+
+sub Generate($$) {
+    my $master_hash = shift;
+    my $name = shift;
+
+    my $obj_hash = $master_hash->{$name};
+
+    generate_table($obj_hash);
+
+    generate_dummy_data( $master_hash, $name );
+
+}
+
+sub generate_dummy_data($$) {
+    my $master_hash = shift;
+    my $name = shift;
+    
+    my $obj_hash = $master_hash->{$name};
+
+    my $PI = 3.14;
+
+    my $name = $obj_hash->{name};
+    my $type_num = $obj_hash->{type_num};
+
+    my $FHD = PreComp::Utilities::OpenFile( $ENV{INSTANCE_ROOT}."/data/default_set/$name.sql" );
+
+    #print_licence_header( $FHD );
+
+    print $FHD "delete from $name;\n";
+    print $FHD "insert into object values ( $type_num, $type_num, \"$name"."_$type_num\", 0, 1 );\n";
+    print $FHD "insert into $name values ( $type_num";
+
+    foreach $key ( keys %{$obj_hash->{data}->{static}} ) {
+
+        print $FHD ", $PI";
+    }
+
+    foreach $key ( keys %{$obj_hash->{data}->{sub}} ) {
+
+        print $FHD ", $master_hash->{$key}->{type_num}";
+
+    }
+
+    print $FHD " );\n";
+
+    PreComp::Utilities::CloseFile();
+}
+
+sub generate_table($) {
     my $obj_hash = shift;
 
     my $name = $obj_hash->{name};
