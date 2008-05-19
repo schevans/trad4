53af4fecc25634f9dca19be05f59601d3fa1d1aa
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-05-19 16:16:53 +0000

    Removed the fpointers from the header => now on object_type_struct_t.

--- trunk/trad4/PreComp/Constants.pm
+++ trunk/trad4/PreComp/Constants.pm
@@ -15,8 +15,6 @@ sub CommomHeader {
         "ulong last_published",
         "int id",
         "object_status status",
-        "void (*calculator_fpointer)(obj_loc_t,int)",
-        "int (*need_refresh_fpointer)(obj_loc_t,int)",
         "int type" ,
         "char name[OBJECT_NAME_LEN]",
         "int log_level"
