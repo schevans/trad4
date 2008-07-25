4393704089c39f2e113451eedea5492d0311ac29
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-07-25 13:29:06 +0000

    Started to add support for multidimensional types, but this is all likely to change.

--- trad4/PreComp/Calculate.pm
+++ trad4/PreComp/Calculate.pm
@@ -27,6 +27,9 @@ sub Generate($) {
     #print_licence_header( $FHD );
 
     print $FHD "\n";
+    print $FHD "// Please see the comment at the top of $ENV{APP}/gen/objects/$name"."_macros.c\n";
+    print $FHD "//  to see what's in-scope.\n";
+    print $FHD "\n";
     print $FHD "#include <iostream>\n";
     print $FHD "\n";
     print $FHD "#include \"$name"."_wrapper.c\"\n";
@@ -35,16 +38,27 @@ sub Generate($) {
     print $FHD "\n";
     print $FHD "void calculate_$name( obj_loc_t obj_loc, int id )\n";
     print $FHD "{\n";
-    print $FHD "    DEBUG( \"calculate_$name( \" << id << \")\" )\n";
+    print $FHD "    DEBUG( \"calculate_$name( \" << id << \" )\" )\n";
     print $FHD "}\n";
     print $FHD "\n";
 
     if ( %{$obj_hash->{data}->{static_vec}} ) {
 
         print $FHD "\n";
-        print $FHD "void extra_loader( obj_loc_t obj_loc, int id, sqlite3* db )\n";
+        print $FHD "static int $name"."_extra_loader_callback(void *obj_loc_v, int argc, char **row, char **azColName)\n";
         print $FHD "{\n";
-        print $FHD "    cout << \"extra_loader()\" << endl;\n";
+        print $FHD "/*\n";
+        print $FHD "    // Have to cast to unsigned char** here as C++ doesn't like\n";
+        print $FHD "    //  void* arithmetic for some strange reason...\n";
+        print $FHD "    unsigned char** obj_loc = (unsigned char**)obj_loc_v;\n";
+        print $FHD "\n";
+        print $FHD "    int id = atoi(row[0]);\n";
+        print $FHD "*/\n";
+        print $FHD "}\n";
+        print $FHD "\n";
+        print $FHD "void $name"."_extra_loader( obj_loc_t obj_loc, int id, sqlite3* db )\n";
+        print $FHD "{\n";
+        print $FHD "    cout << \"$name"."_extra_loader()\" << endl;\n";
         print $FHD "\n";
         print $FHD "}\n";
     }
