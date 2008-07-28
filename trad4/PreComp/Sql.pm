63fbdbc1dd9883023ed468a535e26fe06b542792
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-07-28 19:13:12 +0000

    Generated dummy data. Only one row for the matrices as I can't guess the length of your arrays without parsing common.h, but this may change.

--- trad4/PreComp/Sql.pm
+++ trad4/PreComp/Sql.pm
@@ -11,6 +11,7 @@ use Data::Dumper;
 sub generate_table($);
 sub generate_dummy_data($$);
 sub generate_vec_tables($$);
+sub generate_vec_dummy_data($$$);
 
 sub Generate($$$) {
     my $master_hash = shift;
@@ -21,12 +22,14 @@ sub Generate($$$) {
 
     generate_table($obj_hash);
 
-    if ( $obj_hash->{data}->{static_vec} ) {
+    generate_dummy_data( $master_hash, $name );
+
+    if ( defined $obj_hash->{data}->{static_vec} ) {
 
         generate_vec_tables( $obj_hash, $struct_hash );
+        generate_vec_dummy_data( $master_hash, $name, $struct_hash );
     }
 
-    generate_dummy_data( $master_hash, $name );
 }
 
 sub generate_dummy_data($$) {
@@ -87,7 +90,7 @@ sub generate_vec_tables($$) {
 
             foreach $struct ( keys %{$struct_hash->{$static_vec_type}} ) {
 
-                print $FHD ",\n    $struct_hash->{$static_vec_type}->{$struct} $struct";
+                print $FHD ",\n    $struct ".PreComp::Utilities::Type2Sql( $struct_hash->{$static_vec_type}->{$struct} );
             }
         }
         else {
@@ -134,6 +137,55 @@ sub generate_table($) {
 
 
     PreComp::Utilities::CloseFile();
+}
+
+sub generate_vec_dummy_data($$$) {
+    my $master_hash = shift;
+    my $name = shift;
+    my $struct_hash = shift;
+
+    my $obj_hash = $master_hash->{$name};
+
+    my $PI = 3.14;
+
+    my $type_num = $obj_hash->{type_num};
+
+    foreach $key ( keys %{$obj_hash->{data}->{static_vec}} ) {
+
+        $static_vec_short = $key;
+        $static_vec_short =~ s/\[.*\]//g;
+
+        my $name = $obj_hash->{name}."_".$static_vec_short;
+
+        my $static_vec_type = $obj_hash->{data}->{static_vec}->{$key};
+
+        my $FHD = PreComp::Utilities::OpenFile( $ENV{APP_ROOT}."/data/default_set/"."$name.sql" );
+
+        print $FHD "delete from $name;\n";
+        print $FHD "insert into object values ( $type_num, $type_num, \"$name"."_$type_num\", 0, 1 );\n";
+        print $FHD "insert into $name values ( $type_num";
+
+        if ( $struct_hash->{$static_vec_type} ) {
+
+            foreach $struct ( keys %{$struct_hash->{$static_vec_type}} ) {
+
+                if ( $struct_hash->{$static_vec_type}->{$struct} =~ /int/ ) {
+                    print $FHD ", 1";
+                } 
+                else {
+                    print $FHD ", $PI";
+                }
+            }
+        }
+        else {
+            print $FHD ", $PI";
+        }
+
+        print $FHD " );\n";
+
+        PreComp::Utilities::CloseFile();
+
+    }
 }
 
 1;
