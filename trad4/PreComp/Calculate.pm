7187fb81940bede0fb12282536c508230f0daf91
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-09-16 19:32:31 +0000

    Removed redundant extra_loader stuff - replaced by native structs.

--- trad4/PreComp/Calculate.pm
+++ trad4/PreComp/Calculate.pm
@@ -42,27 +42,6 @@ sub Generate($) {
     print $FHD "}\n";
     print $FHD "\n";
 
-    if ( %{$obj_hash->{data}->{static_vec}} ) {
-
-        print $FHD "\n";
-        print $FHD "static int $name"."_extra_loader_callback(void *obj_loc_v, int argc, char **row, char **azColName)\n";
-        print $FHD "{\n";
-        print $FHD "/*\n";
-        print $FHD "    // Have to cast to unsigned char** here as C++ doesn't like\n";
-        print $FHD "    //  void* arithmetic for some strange reason...\n";
-        print $FHD "    unsigned char** obj_loc = (unsigned char**)obj_loc_v;\n";
-        print $FHD "\n";
-        print $FHD "    int id = atoi(row[0]);\n";
-        print $FHD "*/\n";
-        print $FHD "}\n";
-        print $FHD "\n";
-        print $FHD "void $name"."_extra_loader( obj_loc_t obj_loc, int id, sqlite3* db )\n";
-        print $FHD "{\n";
-        print $FHD "    cout << \"$name"."_extra_loader()\" << endl;\n";
-        print $FHD "\n";
-        print $FHD "}\n";
-    }
-
     PreComp::Utilities::CloseFile();
 }
 
