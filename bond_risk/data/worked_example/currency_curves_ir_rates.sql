a550effa16bf6ad994965b8601a79ca6292bef0e
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-10-21 19:14:09 +0000

    Rationalisation - removed interest_rate_feed and added fx_rate, fixed some static data.

---
+++ bond_risk/data/worked_example/currency_curves_ir_rates.sql
@@ -0,0 +1,12 @@
+delete from currency_curves_ir_rates;
+insert into currency_curves_ir_rates values ( 1, 2.4, 10000 );
+insert into currency_curves_ir_rates values ( 1, 2.5, 10010 );
+insert into currency_curves_ir_rates values ( 1, 2.6, 10100 );
+insert into currency_curves_ir_rates values ( 1, 2.5, 10200 );
+insert into currency_curves_ir_rates values ( 1, 2.5, 10400 );
+insert into currency_curves_ir_rates values ( 1, 2.4, 11000 );
+insert into currency_curves_ir_rates values ( 1, 2.3, 12000 );
+insert into currency_curves_ir_rates values ( 1, 2.3, 13000 );
+insert into currency_curves_ir_rates values ( 1, 2.2, 15000 );
+insert into currency_curves_ir_rates values ( 1, 2.2, 20000 );
+
---
+++ bond_risk/data/worked_example/outright_trade.sql
@@ -0,0 +1,4 @@
+delete from outright_trade;
+insert into object values ( 4, 4, "O38273", 0, 1 );
+insert into outright_trade values ( 4, 102.8, 0, 10000, 9982, 3 );
+
