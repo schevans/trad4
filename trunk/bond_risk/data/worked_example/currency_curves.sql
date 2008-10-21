636421a2a1d594d3856659795fb0192878e01526
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-10-21 19:14:09 +0000

    Rationalisation - removed interest_rate_feed and added fx_rate, fixed some static data.

---
+++ trunk/bond_risk/data/worked_example/currency_curves.sql
@@ -0,0 +1,3 @@
+delete from currency_curves;
+insert into object values ( 1, 1, "LIBOR-USD", 0, 1 ); 
+insert into currency_curves values ( 1 );
---
+++ trunk/bond_risk/data/worked_example/repo_trade.sql
@@ -0,0 +1,4 @@
+delete from repo_trade;
+insert into object values ( 5, 5, "R25745", 0, 1 );
+insert into repo_trade values ( 5, 4.25, 0, 12388, 0, 8934, 23427.34, 25000, 0, 3, 1, 2 );
+
