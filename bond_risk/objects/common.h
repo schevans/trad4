82fe30afff874b83b060d1b409c61ae7e6827340
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-10-21 17:57:40 +0000

    Updated bond_risk - added new constants and enums.

--- bond_risk/objects/common.h
+++ bond_risk/objects/common.h
@@ -7,18 +7,5 @@
 #include <sys/types.h>
 
 
-#define DATE_RANGE_START 10000
-#define DATE_RANGE_END 20000
-
-#define TODAY DATE_RANGE_START
-
-
-#define DATE_RANGE_LEN DATE_RANGE_END - DATE_RANGE_START
-
-#define YEAR_BASIS 360.0
-
-#define INTEREST_RATE_LEN 10
-
-#define DISCOUNT_RATE_LEN DATE_RANGE_LEN
 
 #endif
