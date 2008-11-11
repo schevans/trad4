ac170f35314f82a0114250ebea63bddc9118a0ec
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-11-11 19:39:55 +0000

    New-style sub object update. Had to disable a couple of calculate functions for mostly unrelated reasons.

--- trunk/bond_risk/objects/outright_trade.c
+++ trunk/bond_risk/objects/outright_trade.c
@@ -10,8 +10,8 @@ using namespace std;
 
 void calculate_outright_trade( obj_loc_t obj_loc, int id )
 {
-    outright_trade_pv = ( outright_trade_quantity * bond_price / 100 );
+    outright_trade_pv = ( outright_trade_quantity * sbond_price / 100 );
 
-    outright_trade_pnl = (( outright_trade_quantity * bond_price / 100 ) - ( outright_trade_quantity * outright_trade_trade_price / 100 ));
+    outright_trade_pnl = (( outright_trade_quantity * sbond_price / 100 ) - ( outright_trade_quantity * outright_trade_trade_price / 100 ));
 }
 
