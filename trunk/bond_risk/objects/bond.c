5aac6b03c959c92d9a7070524e30c67001eeac8c
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-11-21 20:59:00 +0000

    Re-enabled and updated calculate function.

--- trunk/bond_risk/objects/bond.c
+++ trunk/bond_risk/objects/bond.c
@@ -12,10 +12,10 @@ using namespace std;
 
 void calculate_bond( obj_loc_t obj_loc, int id )
 {
-/*
     std::vector<int> _coupon_date_vec;
     std::vector<float> _payment_vec;
-    std::vector<float> _payment_vec_01;
+    std::vector<float> _payment_vec_p01;
+    std::vector<float> _payment_vec_m01;
 
     int days_between_coupons = (int)YEAR_BASIS / bond_coupons_per_year;
     float coupon_discount_rate_per_period = ( bond_coupon / 100 ) / bond_coupons_per_year;
@@ -40,25 +40,28 @@ void calculate_bond( obj_loc_t obj_loc, int id )
     for( iter = _coupon_date_vec.begin() ; iter < _coupon_date_vec.end() ; iter++ )
     {
         _payment_vec.push_back( 100.0 * coupon_discount_rate_per_period * scurrency_curves_discount_rate[*iter - TODAY] );
-        _payment_vec_01.push_back( 100.0 * coupon_discount_rate_per_period * scurrency_curves_discount_rate_01[*iter - TODAY] );
+        _payment_vec_p01.push_back( 100.0 * coupon_discount_rate_per_period * scurrency_curves_discount_rate_p01[*iter - TODAY] );
+        _payment_vec_m01.push_back( 100.0 * coupon_discount_rate_per_period * scurrency_curves_discount_rate_m01[*iter - TODAY] );
     }
 
     _payment_vec.push_back( 100 * scurrency_curves_discount_rate[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
-    _payment_vec_01.push_back( 100 * scurrency_curves_discount_rate_01[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
+    _payment_vec_p01.push_back( 100 * scurrency_curves_discount_rate_p01[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
+    _payment_vec_m01.push_back( 100 * scurrency_curves_discount_rate_m01[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
 
     float price(0.0);
-    float price_01(0.0);
+    float price_p01(0.0);
+    float price_m01(0.0);
 
     for( unsigned int i = 0 ; i < _payment_vec.size() ; i++ )
     {
         price = price + _payment_vec[i];
 
-        price_01 = price_01 + _payment_vec_01[i];
+        price_p01 = price_p01 + _payment_vec_p01[i];
+        price_m01 = price_m01 + _payment_vec_m01[i];
 
     }
 
     bond_price = ( price );
-    bond_dv01 = ( price - price_01 );
-*/
+    bond_pv01 = ( price_p01 - price_m01 ) / 2.0;
 }
 
