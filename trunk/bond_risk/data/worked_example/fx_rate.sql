314b5474479083c8b45e65720b55dfdcdf8327d2
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-11-22 13:11:53 +0000

    Generalising fx_rate.

--- trunk/bond_risk/data/worked_example/fx_rate.sql
+++ trunk/bond_risk/data/worked_example/fx_rate.sql
@@ -1,3 +1,3 @@
 delete from fx_rate;
-insert into object values ( 2, 2, "GBP/USD", 0, 1 );
-insert into fx_rate values ( 2, 1.6941 );
+insert into object values ( 2, 2, "GBP-USD", 0, 1 );
+insert into fx_rate values ( 2, 1.6941, 2, 1 );
