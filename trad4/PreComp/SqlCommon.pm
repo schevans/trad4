01b9c5bbf4a830850ae107a9646924acfff3d361
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-11-27 20:45:24 +0000

    Added basic indices and had a clean-up.

--- trad4/PreComp/SqlCommon.pm
+++ trad4/PreComp/SqlCommon.pm
@@ -4,36 +4,55 @@
 
 package PreComp::SqlCommon;
 
+use warnings;
+use strict;
+
 use PreComp::Utilities;
 use PreComp::Constants;
 use Data::Dumper;
 
 sub generate_object_types($);
+sub generate_indices($);
 
 sub Generate($) {
     my $master_hash = shift;
 
     generate_object_types( $master_hash );
+
+    generate_indices( $master_hash );
+}
+
+sub generate_indices($) {
+    my $master_hash = shift;
+
+    my $FHD = PreComp::Utilities::OpenFile( PreComp::Constants::SqlRoot()."indices.sql" );
+
+    my $object_type;
+
+    print $FHD "create index object_1 on object (id);\n";
+
+    foreach $object_type ( keys %{$master_hash} ) {
+
+        print $FHD "create index $object_type"."_1 on $object_type (id);\n";
+    }
+
+    PreComp::Utilities::CloseFile();
 }
 
 sub generate_object_types($) {
     my $master_hash = shift;
     
-    my $obj_hash = $master_hash->{$name};
-
     my $FHD = PreComp::Utilities::OpenFile( PreComp::Constants::SqlRoot()."object_types.sql" );
 
     #print_licence_header( $FHD );
 
-    my $type;
+    my $object_type;
 
     print $FHD "delete from object_types;\n";
 
-    foreach $type ( %{$master_hash} ) {
+    foreach $object_type ( keys %{$master_hash} ) {
 
-        if ( $master_hash->{$type} ) {  # Hack alert - my hashes are a bit of a mess.
-            print $FHD "insert into object_types values ( $master_hash->{$type}->{type_num}, \"$master_hash->{$type}->{name}\", $master_hash->{$type}->{tier}, 1 );\n";
-        }
+        print $FHD "insert into object_types values ( $master_hash->{$object_type}->{type_num}, \"$master_hash->{$object_type}->{name}\", $master_hash->{$object_type}->{tier}, 1 );\n";
     }
 
     PreComp::Utilities::CloseFile();
