1f149791a6558497db82d1d43f52af05388dd340
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2008-11-30 21:56:39 +0000

    Fixed printing. Still not happy about this as it takes far to much knowledge about the trad4 internals. I should really expedite that feed server I've been thinking about..

--- calculus/objects/printer.c
+++ calculus/objects/printer.c
@@ -2,13 +2,23 @@
 #include <iostream>
 #include "trad4.h"
 
+#include "constants.h"
+#include "d2f.h"
+#include "d2f_macros.h"
+#include "df.h"
+#include "df_macros.h"
+#include "enums.h"
+#include "f.h"
+#include "f_macros.h"
+#include "structures.h"
+
+
 using namespace std;
 
-extern "C" void printer( obj_loc_t obj_loc )
+extern "C" void printer( obj_loc_t obj_loc, tier_manager_t tier_manager )
 {
     cout << "Printing.." << endl;
 
-/*
     int id;
 
 cout << "TM: " << tier_manager[1][0] << endl;
@@ -56,5 +66,5 @@ cout << "TM: " << tier_manager[1][0] << endl;
     }
 
     cout << endl;
-*/
+
 }
