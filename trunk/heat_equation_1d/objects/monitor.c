ebd25433e4bdbbc5ca1f42e05c2a52e7237fff64
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-01-23 14:49:10 +0000

    Detect divergence. Confusing logic in here but I might have made a meal out of it.

--- trunk/heat_equation_1d/objects/monitor.c
+++ trunk/heat_equation_1d/objects/monitor.c
@@ -14,19 +14,34 @@ using namespace std;
 void calculate_monitor( obj_loc_t obj_loc, int id )
 {
     int converged = 1;
+    int diverged = 0;
+
+    double converged_limit =  1.0e-6;
+    double diverged_limit =  1.0;
 
     for ( int i=0 ; i < NUM_CHANGES ; i++ )
     {
-        if ( fabs( monitor_my_changes_change(i) ) > 1.0e-6 )
+        if ( fabs( monitor_my_changes_change(i) ) > diverged_limit )
+        {
+            diverged = 1;
+            break;
+        }
+
+        if ( converged && fabs( monitor_my_changes_change(i) ) > converged_limit )
         {
             converged = 0; 
-            break;
         }
     }
 
     if ( converged )
     {
         cout << "Converged in " << monitor_counter << " Steps" << endl;
+        exit(0);
+    }
+
+    if ( diverged )
+    {
+        cout << "Diverged in " << monitor_counter << " Steps" << endl;
         exit(0);
     }
 
