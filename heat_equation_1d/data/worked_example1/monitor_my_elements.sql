d7d4e9204eb8ce25b99ef1a21aa08166ec7426cd
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-01-23 16:43:34 +0000

    Added elements to monitor and renamed constant.

---
+++ heat_equation_1d/data/worked_example1/monitor_my_elements.sql
@@ -0,0 +1,12 @@
+delete from monitor_my_elements;
+insert into monitor_my_elements values ( 99998, 1 );
+insert into monitor_my_elements values ( 99998, 2 );
+insert into monitor_my_elements values ( 99998, 3 );
+insert into monitor_my_elements values ( 99998, 4 );
+insert into monitor_my_elements values ( 99998, 5 );
+insert into monitor_my_elements values ( 99998, 6 );
+insert into monitor_my_elements values ( 99998, 7 );
+insert into monitor_my_elements values ( 99998, 8 );
+insert into monitor_my_elements values ( 99998, 9 );
+insert into monitor_my_elements values ( 99998, 10 );
+
