79a3e8b2c429ff489c55fc9ec0cfb7052904e24c
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-01-29 14:51:47 +0000

    Moved converged_ and diverged_limits onto monitor static rather than hard-coded. The diverged_limi t should really be derived from the step_size I think, but won't be in v1.0.

--- heat_equation_1d/data/worked_example1/monitor.sql
+++ heat_equation_1d/data/worked_example1/monitor.sql
@@ -1,3 +1,3 @@
 delete from monitor;
 insert into object values ( 99998, 4, "monitor", 0, 1 );
-insert into monitor values ( 99998, 0, 0, 99999 );
+insert into monitor values ( 99998, 0, 0, 1e-06, 1, 99999 );
