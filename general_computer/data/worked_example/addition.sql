6ec48e009666370baf216797fddb3b3dd29f4e3c
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-05 16:21:31 +0000

    New app.

---
+++ general_computer/data/worked_example/addition.sql
@@ -0,0 +1,5 @@
+delete from addition;
+insert into object values ( 1, 2, 1, "addition_1", 0, 1 );
+insert into addition values ( 1, 2, 2 );
+insert into object values ( 2, 2, 2, "addition_2", 0, 1 );
+insert into addition values ( 2, 1, 1 );
---
+++ general_computer/objects/addition.c
@@ -0,0 +1,28 @@
+
+// Please see the comment at the top of test_suite/gen/objects/addition_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "addition_wrapper.c"
+
+using namespace std;
+
+void calculate_addition( obj_loc_t obj_loc, int id )
+{
+    // Write me.
+
+    
+
+    if ( numeric1_output == 0 ) {
+        
+        addition_output = 1;
+    }
+    else {
+
+        addition_output = numeric1_output + numeric2_output;
+    }
+
+cout << addition_output << endl;
+}
+
