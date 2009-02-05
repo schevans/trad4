c5b33589ab9a2db57f5db479a44f83e1b2608e15
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-05 16:21:31 +0000

    New app.

---
+++ trunk/general_computer/objects/numeric.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of test_suite/gen/objects/numeric_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "numeric_wrapper.c"
+
+using namespace std;
+
+void calculate_numeric( obj_loc_t obj_loc, int id )
+{
+    // Write me.
+}
+
