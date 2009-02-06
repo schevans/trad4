a0fd8901b930405b04b8e611096e4854face1588
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-06 14:18:32 +0000

    IoI Phase2 changes.

--- trunk/bond_risk/data/worked_example/outright_trade.sql
+++ trunk/bond_risk/data/worked_example/outright_trade.sql
@@ -1,4 +1,4 @@
 delete from outright_trade;
-insert into object values ( 4, 4, "O38273", 0, 1 );
+insert into object values ( 4, 4, 3, "O38273", 0, 1 );
 insert into outright_trade values ( 4, 102.8, 0, 10000, 9982, 3 );
 
