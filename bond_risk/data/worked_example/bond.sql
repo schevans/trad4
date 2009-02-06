7236624b11a6f2c2e38584597b65034dd95997dc
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-06 14:18:32 +0000

    IoI Phase2 changes.

--- bond_risk/data/worked_example/bond.sql
+++ bond_risk/data/worked_example/bond.sql
@@ -1,4 +1,4 @@
 delete from bond;
-insert into object values ( 3, 3, "US81823453", 0, 1 );
+insert into object values ( 3, 3, 2, "US81823453", 0, 1 );
 insert into bond values ( 3, 15000, 5000, 1, 2.8, 1 );
 
