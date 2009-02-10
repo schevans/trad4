cc3ed9b8b181f095a04f2a394ed016fb5ce01d0c
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-10 19:19:45 +0000

    Added operators cume_norm_dist, natural_log and square_root.

---
+++ trunk/general_computer/objects/cume_norm_dist.c
@@ -0,0 +1,40 @@
+
+// Please see the comment at the top of general_computer/gen/objects/cume_norm_dist_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+#include <math.h>
+
+
+#include "cume_norm_dist_wrapper.c"
+
+using namespace std;
+
+void calculate_cume_norm_dist( obj_loc_t obj_loc, int id )
+{
+
+    double x = numeric1_output;
+
+    const double b1 =  0.319381530;
+    const double b2 = -0.356563782;
+    const double b3 =  1.781477937;
+    const double b4 = -1.821255978;
+    const double b5 =  1.330274429;
+    const double p  =  0.2316419;
+    const double c  =  0.39894228;
+
+    if ( x >= 0.0 ) 
+    {
+
+        double t = 1.0 / ( 1.0 + p * x );
+        cume_norm_dist_output = (1.0 - c * exp( -x * x / 2.0 ) * t * ( t *( t * ( t * ( t * b5 + b4 ) + b3 ) + b2 ) + b1 ));
+    }
+    else 
+    {
+    
+        double t = 1.0 / ( 1.0 - p * x );
+        cume_norm_dist_output = ( c * exp( -x * x / 2.0 ) * t * ( t *( t * ( t * ( t * b5 + b4 ) + b3 ) + b2 ) + b1 ));
+    }
+
+}
+
---
+++ trunk/general_computer/objects/natural_log.c
@@ -0,0 +1,16 @@
+
+// Please see the comment at the top of general_computer/gen/objects/natural_log_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+#include <math.h>
+
+#include "natural_log_wrapper.c"
+
+using namespace std;
+
+void calculate_natural_log( obj_loc_t obj_loc, int id )
+{
+    natural_log_output = log( numeric1_output );
+}
+
