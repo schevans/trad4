a783c2ad0a27aa23ca040e883076e2d0e71a4d04
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-10 19:17:16 +0000

    Missed checkin: Added operators minus, div and mult. Also hacked in a feed type.

---
+++ general_computer/objects/division.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of general_computer/gen/objects/division_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "division_wrapper.c"
+
+using namespace std;
+
+void calculate_division( obj_loc_t obj_loc, int id )
+{
+    division_output = numeric1_output / numeric2_output;
+}
+
---
+++ general_computer/objects/subtraction.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of general_computer/gen/objects/subtraction_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "subtraction_wrapper.c"
+
+using namespace std;
+
+void calculate_subtraction( obj_loc_t obj_loc, int id )
+{
+    subtraction_output = numeric1_output - numeric2_output;
+}
+
