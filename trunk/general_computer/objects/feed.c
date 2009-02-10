54bec577cef99c036160c7e4c24ccec8c44ebcc8
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-10 19:17:16 +0000

    Missed checkin: Added operators minus, div and mult. Also hacked in a feed type.

---
+++ trunk/general_computer/objects/feed.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of general_computer/gen/objects/feed_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "feed_wrapper.c"
+
+using namespace std;
+
+void calculate_feed( obj_loc_t obj_loc, int id )
+{
+    feed_output = feed_value;
+}
+
---
+++ trunk/general_computer/objects/multiplication.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of general_computer/gen/objects/multiplication_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "multiplication_wrapper.c"
+
+using namespace std;
+
+void calculate_multiplication( obj_loc_t obj_loc, int id )
+{
+    multiplication_output = numeric1_output * numeric2_output;
+}
+
