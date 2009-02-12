b0d20334a205f116ac99955af4913a7cd946bc8a
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-12 19:06:41 +0000

    Added licence & copyright.

--- trunk/general_computer/objects/addition.c
+++ trunk/general_computer/objects/addition.c
@@ -1,3 +1,6 @@
+// Copyright (c) Steve Evans 2009
+// steve@topaz.myzen.co.uk
+// This code is released under the BSD licence. For details see $APP_ROOT/LICENCE
 
 // Please see the comment at the top of test_suite/gen/objects/addition_macros.c
 //  to see what's in-scope.
--- trunk/general_computer/objects/division.c
+++ trunk/general_computer/objects/division.c
@@ -1,3 +1,6 @@
+// Copyright (c) Steve Evans 2009
+// steve@topaz.myzen.co.uk
+// This code is released under the BSD licence. For details see $APP_ROOT/LICENCE
 
 // Please see the comment at the top of general_computer/gen/objects/division_macros.c
 //  to see what's in-scope.
