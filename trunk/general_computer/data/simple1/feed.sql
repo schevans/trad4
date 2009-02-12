18125718a801e67aa1ca16d7795b91599774cc46
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-12 16:30:11 +0000

    Added simple1 z = (a+b) + (c+d) data set.

---
+++ trunk/general_computer/data/simple1/feed.sql
@@ -0,0 +1,9 @@
+delete from feed;
+insert into object values ( 1, 2, 1, "a", 1, 1 );
+insert into feed values ( 1, 8, 1, 1 );
+insert into object values ( 2, 2, 1, "b", 1, 1 );
+insert into feed values ( 2, 3.5, 1, 1 );
+insert into object values ( 3, 2, 1, "c", 1, 1 );
+insert into feed values ( 3, 7.3, 1, 1 );
+insert into object values ( 4, 2, 1, "d", 1, 1 );
+insert into feed values ( 4, 11, 1, 1 );
