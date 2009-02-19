5a8450fc1a7bb9d742260601fb0c0c82a0c60772
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-19 20:36:02 +0000

    Removed hacked starting values.

--- the_larch/objects/trunk.c
+++ the_larch/objects/trunk.c
@@ -10,16 +10,7 @@ using namespace std;
 
 void calculate_trunk( obj_loc_t obj_loc, int id )
 {
-
-    if ( trunk_glucose == 0 && trunk_water == 0 )
-    {
-        trunk_glucose = 10;
-        trunk_water = 10;
-    }
-    else
-    {
-        trunk_glucose = trunk_glucose + my_crown_glucose - my_roots_glucose_taken;
-        trunk_water = trunk_water + my_roots_water - my_crown_water_taken;
-    }
+    trunk_glucose = trunk_glucose + my_crown_glucose - my_roots_glucose_taken;
+    trunk_water = trunk_water + my_roots_water - my_crown_water_taken;
 }
 
