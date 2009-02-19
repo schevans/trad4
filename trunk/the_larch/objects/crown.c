66b87c0063c0d38971ae51fb1dc4c8e8bb5ae5e9
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-19 21:46:31 +0000

    Crown now grows more leaves until the tree dies due to lack of water: Need to grow roots too.

--- trunk/the_larch/objects/crown.c
+++ trunk/the_larch/objects/crown.c
@@ -3,6 +3,7 @@
 //  to see what's in-scope.
 
 #include <iostream>
+#include <math.h>
 
 #include "crown_wrapper.c"
 
@@ -10,7 +11,6 @@ using namespace std;
 
 void calculate_crown( obj_loc_t obj_loc, int id )
 {
-
     if ( crown_num_leaves == 0 )
     {
         crown_num_leaves = crown_start_num_leaves;
@@ -18,6 +18,18 @@ void calculate_crown( obj_loc_t obj_loc, int id )
 
     crown_water_taken = crown_water_absorbed_per_leaf * crown_num_leaves;
 
-    crown_glucose = ( crown_water_absorbed_per_leaf + crown_light_absorbed_per_leaf + crown_co2_absorbed_per_leaf ) * crown_num_leaves;
+    double local_glucose_produced = ( crown_water_absorbed_per_leaf + crown_light_absorbed_per_leaf + crown_co2_absorbed_per_leaf ) * crown_num_leaves;
+
+    crown_glucose_published = local_glucose_produced * crown_glucose_published_coeff;
+
+    crown_glucose_store = crown_glucose_store + ( local_glucose_produced - crown_glucose_published );
+
+    if ( crown_glucose_store > crown_glucose_per_leaf )
+    {
+        int local_num_new_leaves = (int)(( crown_glucose_store - fmod( crown_glucose_store, crown_glucose_per_leaf ) ) / crown_glucose_per_leaf);
+
+        crown_num_leaves = crown_num_leaves + local_num_new_leaves;
+        crown_glucose_store = crown_glucose_store - ( crown_glucose_per_leaf * local_num_new_leaves );
+    }
 }
 
--- trunk/the_larch/objects/trunk.c
+++ trunk/the_larch/objects/trunk.c
@@ -10,7 +10,19 @@ using namespace std;
 
 void calculate_trunk( obj_loc_t obj_loc, int id )
 {
-    trunk_glucose = trunk_glucose + my_crown_glucose - my_roots_glucose_taken;
+    trunk_glucose = trunk_glucose + my_crown_glucose_published - my_roots_glucose_taken;
     trunk_water = trunk_water + my_roots_water - my_crown_water_taken;
+
+    if ( trunk_water < 0.0 )
+    {
+        cout << "Run out of water! Tree dies." << endl;
+        exit(0);
+    }
+
+    if ( my_trunk_glucose < 0.0 )
+    {
+        cout << "Run out of glucose! Tree dies." << endl;
+        exit(0);
+    }
 }
 
