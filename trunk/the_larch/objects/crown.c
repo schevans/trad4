0d78424fbb2c3b74bae323773422f02fe7d5b4b8
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-19 20:38:31 +0000

    Adding leaves - contained in crown.

--- trunk/the_larch/objects/crown.c
+++ trunk/the_larch/objects/crown.c
@@ -10,7 +10,14 @@ using namespace std;
 
 void calculate_crown( obj_loc_t obj_loc, int id )
 {
-    crown_water_taken = 0.5;
-    crown_glucose = crown_co2 + crown_water_taken + crown_light;
+
+    if ( crown_num_leaves == 0 )
+    {
+        crown_num_leaves = crown_start_num_leaves;
+    }
+
+    crown_water_taken = crown_water_absorbed_per_leaf * crown_num_leaves;
+
+    crown_glucose = ( crown_water_absorbed_per_leaf + crown_light_absorbed_per_leaf + crown_co2_absorbed_per_leaf ) * crown_num_leaves;
 }
 
