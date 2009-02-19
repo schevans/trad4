472c75f91815f0aa64dcaf963f89f66f83785518
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-19 16:50:28 +0000

    And now for something completely different..

---
+++ trunk/the_larch/data/worked_example1/crown.sql
@@ -0,0 +1,3 @@
+delete from crown;
+insert into object values ( 3, 3, 1, "crown_3", 1, 1 );
+insert into crown values ( 3, 1.5, 1.5, 2 );
---
+++ trunk/the_larch/data/worked_example1/trunk.sql
@@ -0,0 +1,3 @@
+delete from trunk;
+insert into object values ( 2, 2, 2, "trunk_2", 1, 1 );
+insert into trunk values ( 2, 3, 1 );
