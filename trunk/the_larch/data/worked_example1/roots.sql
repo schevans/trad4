1382386186a32230fa1b81476b8e36d6c97a54ac
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-20 16:45:15 +0000

    Roots now grow rootlets.

--- trunk/the_larch/data/worked_example1/roots.sql
+++ trunk/the_larch/data/worked_example1/roots.sql
@@ -1,3 +1,3 @@
 delete from roots;
 insert into object values ( 1, 1, 1, "roots_1", 0, 1 );
-insert into roots values ( 1, 0.5, 2 );
+insert into roots values ( 1, 1.2, 0.4, 1, 2 );
--- trunk/the_larch/objects/roots.c
+++ trunk/the_larch/objects/roots.c
@@ -3,6 +3,7 @@
 //  to see what's in-scope.
 
 #include <iostream>
+#include <math.h>
 
 #include "roots_wrapper.c"
 
@@ -10,6 +11,24 @@ using namespace std;
 
 void calculate_roots( obj_loc_t obj_loc, int id )
 {
-    roots_glucose_taken = 0.5;
+    if ( roots_num_rootlets == 0 )
+    {
+        roots_num_rootlets = roots_start_num_rootlets;
+    }
+
+    roots_water_published = roots_water_absorbed_per_rootlet * roots_num_rootlets;
+
+    roots_glucose_taken = my_trunk_glucose;
+
+    roots_glucose_store = roots_glucose_store + roots_glucose_taken;
+
+    if ( roots_glucose_store > roots_glucose_per_rootlet )
+    {
+        int local_num_new_rootlets = (int)(( roots_glucose_store - fmod( roots_glucose_store, roots_glucose_per_rootlet ) ) / roots_glucose_per_rootlet );
+
+        roots_num_rootlets = roots_num_rootlets + local_num_new_rootlets;
+
+        roots_glucose_store = roots_glucose_store - ( roots_glucose_per_rootlet * local_num_new_rootlets );
+    }
 }
 
