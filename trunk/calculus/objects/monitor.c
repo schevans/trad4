252bd2427e53e253f6f3d662555655ba13423cd2
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-22 18:21:38 +0000

    Flipped monitor output round into columns and output the data to a csv file.

--- trunk/calculus/objects/monitor.c
+++ trunk/calculus/objects/monitor.c
@@ -3,6 +3,7 @@
 //  to see what's in-scope.
 
 #include <iostream>
+#include <fstream> 
 
 #include "monitor_wrapper.c"
 
@@ -11,6 +12,33 @@ using namespace std;
 void calculate_monitor( obj_loc_t obj_loc, int id )
 {
 
+    std::ostringstream filename;
+    filename << getenv("APP_ROOT") << "/" << getenv("APP") << "_" << NUM_NODES << ".csv";
+
+    ofstream outfile( filename.str().c_str() );
+
+    if ( !outfile ) {
+
+        cout << "monitor can't open " << filename << " for writing." << endl;
+        exit(1);
+    }
+
+    outfile << "x,y,df,d2f" << endl;
+
+    for ( int i=0 ; i < NUM_NODES ; i++ )
+    {
+        outfile << monitor_my_f_x( i ) << ","
+            << monitor_my_f_y( i ) << ","
+            << monitor_my_df_df( i ) << ","
+            << monitor_my_d2f_d2f( i ) << endl;
+    }
+
+    outfile.close();
+
+    cout << "Data written to " << filename.str() << endl;
+
+    
+/*
     cout << "x,";
 
     for ( int i=0 ; i < NUM_NODES ; i++ )
@@ -46,5 +74,6 @@ void calculate_monitor( obj_loc_t obj_loc, int id )
     }
 
     cout << endl;
+*/
 }
 
