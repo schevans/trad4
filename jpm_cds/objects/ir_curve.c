ce06940f0cc54ef27688e7ebfe544f3002622b93
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-02-28 20:27:20 +0000

    Init.

---
+++ jpm_cds/objects/ir_curve.c
@@ -0,0 +1,15 @@
+
+// Please see the comment at the top of jpm_cds/gen/objects/ir_curve_macros.c
+//  to see what's in-scope.
+
+#include <iostream>
+
+#include "ir_curve_wrapper.c"
+
+using namespace std;
+
+void calculate_ir_curve( obj_loc_t obj_loc, int id )
+{
+    // Write me.
+}
+
