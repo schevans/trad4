76c44324e22fd741c160e9790928bd267f30e798
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-03-04 10:27:26 +0000

    Hooked up char badDayConv, now that chars are supported.

--- jpm_cds/data/BuildExampleZeroCurve/ir_curve.sql
+++ jpm_cds/data/BuildExampleZeroCurve/ir_curve.sql
@@ -1,3 +1,3 @@
 delete from ir_curve;
 insert into object values ( 1, 1, 1, "ir_curve_1", 1, 1 );
-insert into ir_curve values ( 1, 148655, 3, 4, 2, 1, 1 );
+insert into ir_curve values ( 1, 148655, 3, 4, 2, 'M', 1 );
