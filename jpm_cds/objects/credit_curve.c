f7d16deb97687af741339f0d21d3f71447803d8f
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-03-04 13:54:14 +0000

    Cleaned up.

--- jpm_cds/objects/credit_curve.c
+++ jpm_cds/objects/credit_curve.c
@@ -24,68 +24,25 @@ using namespace std;
 
 void calculate_credit_curve( obj_loc_t obj_loc, int id )
 {
-
-//if (JpmcdsErrMsgEnableRecord(20, 128) != SUCCESS)
-//exit(0);
-
-TDate *endDates;
-double *couponRates;
-
-    endDates = NEW_ARRAY(TDate, credit_curve_nbDate);
-    couponRates = NEW_ARRAY( double, credit_curve_nbDate );
-
-    int i2;
-    for ( i2=0 ; i2 < credit_curve_nbDate ; i2++ )
-    {
-        endDates[i2] = credit_curve_today + ( i2 * 365 );
-
-
-        couponRates[i2] = 0.05;
-    }
-
-    TCurve *tmp = 0;
-       
-    tmp = (TCurve*)(&s_ir_curve_pTCurve);
- 
-    cout << "s_ir_curve_baseDate: " << s_ir_curve_baseDate << endl;
-    cout << "s_ir_curve_pTCurve->fNumItems: " << s_ir_curve_pTCurve.fNumItems << endl;
-
     char* calendar = "NONE";
 
-    TDate *local_endDates;
-    local_endDates = NEW_ARRAY(TDate, credit_curve_nbDate);
-
-    TBoolean local_includes = FALSE;
-
-    cout << "credit_curve_endDates[3]: " << credit_curve_endDates[3] << endl;
-
-    cout << "credit_curve_startDate: " << credit_curve_startDate << endl;
-
-    TDateInterval couponInterval;
-    couponInterval.prd = 1;
-    couponInterval.prd_typ = 'A';
-    couponInterval.flag = 0;
-
-
-     credit_curve_pTCurve = *(JpmcdsCleanSpreadCurve(
+    credit_curve_pTCurve = *(JpmcdsCleanSpreadCurve(
         credit_curve_today,
         &s_ir_curve_pTCurve,
         credit_curve_startDate,
         credit_curve_stepinDate,
         credit_curve_cashSettleDate,
         credit_curve_nbDate,
-        endDates,
+        credit_curve_endDates,
         credit_curve_couponRates,
-        &local_includes,
+        credit_curve_includes,
         credit_curve_recoveryRate,
         credit_curve_payAccOnDefault,
-        &couponInterval,
+        credit_curve_couponInterval,
         credit_curve_paymentDcc,
         credit_curve_stubType,
         credit_curve_badDayConv,
         calendar ));
 
-    FREE(local_endDates);
-    
 }
 
