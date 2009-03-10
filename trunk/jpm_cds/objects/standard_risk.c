c8c7319247af41a31cde8257255eca51f6d10118
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-03-10 20:21:26 +0000

    Added crude self-test.

--- trunk/jpm_cds/objects/standard_risk.c
+++ trunk/jpm_cds/objects/standard_risk.c
@@ -3,6 +3,7 @@
 //  to see what's in-scope.
 
 #include <iostream>
+#include <math.h>
 
 #include "standard_risk_wrapper.c"
 
@@ -13,5 +14,12 @@ void calculate_standard_risk( obj_loc_t obj_loc, int id )
     standard_risk_pv = s_contingent_leg_pv - s_fee_leg_pv;
 
     cout << "standard_risk_pv: " << standard_risk_pv << endl;
+
+    if ( fabs(standard_risk_pv - -8934.51) < 0.0001 )
+    {
+        cout << "XXXXXXXXXXXXXXXXXXXXX" << endl;
+        cout << "std pv's changed" << endl;
+        cout << "XXXXXXXXXXXXXXXXXXXXX" << endl;
+    }
 }
 
