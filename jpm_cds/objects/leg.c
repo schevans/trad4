4f52956feabf5c81e676b841fd1f100dd81891da
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-03-16 17:43:33 +0000

    Licence and copyright.

--- jpm_cds/objects/leg.c
+++ jpm_cds/objects/leg.c
@@ -1,3 +1,9 @@
+// Copyright (c) Steve Evans 2009
+// steve@topaz.myzen.co.uk
+// This code is released under the BSD licence. For details see $APP_ROOT/LICENCE
+//
+// This application is based on the ISDA CDS Standard Model (version 1.7),  
+// developed and supported in collaboration with Markit Group Ltd.
 
 // Please see the comment at the top of jpm_cds/gen/objects/leg_macros.c
 //  to see what's in-scope.
