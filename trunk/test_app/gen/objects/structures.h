f72604f077e057c3d4d02a75b138045c560847ce
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-04-16 19:40:13 +0000

    Testing.

---
+++ trunk/test_app/gen/objects/structures.h
@@ -0,0 +1,30 @@
+
+
+#ifndef __structures_h__
+#define __structures_h__
+
+#include "constants.h"
+#include "aliases.h"
+
+typedef struct {
+    int d;
+    float ha[NUM_RATES];
+} ms_inner;
+
+typedef struct {
+    ms_inner my_inner_ms[3];
+    int g;
+} ms_outer;
+
+typedef struct {
+    double rates[NUM_RATES];
+    date dates[NUM_RATES];
+} struct2;
+
+typedef struct {
+    int x;
+    float y;
+} struct1;
+
+
+#endif
