a009706dd7f4aaf471d44e340a99815e411faff9
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-05-11 19:35:54 +0000

    Added test case.

---
+++ trunk/test_app/data/worked_example/addition.sql
@@ -0,0 +1,3 @@
+delete from addition;
+insert into object values ( 13, 13, 2, "addition13", 1, 1 );
+insert into addition values ( 13, 3.14, 11, 12 );
---
+++ trunk/test_app/data/worked_example/feed.sql
@@ -0,0 +1,5 @@
+delete from feed;
+insert into object values ( 11, 11, 1, "feed_11", 1, 1 );
+insert into feed values ( 11, 7.83, 11, 11 );
+insert into object values ( 12, 11, 1, "feed_12", 1, 1 );
+insert into feed values ( 12, 2.225, 12, 12 );
