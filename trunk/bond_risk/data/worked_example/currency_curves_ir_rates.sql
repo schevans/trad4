2a71d2d472ac261fa7018220822df7e03f0e5558
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-05-13 16:38:14 +0000

    PV3 upgrade.

--- trunk/bond_risk/data/worked_example/currency_curves_ir_rates.sql
+++ trunk/bond_risk/data/worked_example/currency_curves_ir_rates.sql
@@ -1,12 +1,12 @@
 delete from currency_curves_ir_rates;
-insert into currency_curves_ir_rates values ( 1, 2.4, 10000 );
-insert into currency_curves_ir_rates values ( 1, 2.5, 10010 );
-insert into currency_curves_ir_rates values ( 1, 2.6, 10100 );
-insert into currency_curves_ir_rates values ( 1, 2.5, 10200 );
-insert into currency_curves_ir_rates values ( 1, 2.5, 10400 );
-insert into currency_curves_ir_rates values ( 1, 2.4, 11000 );
-insert into currency_curves_ir_rates values ( 1, 2.3, 12000 );
-insert into currency_curves_ir_rates values ( 1, 2.3, 13000 );
-insert into currency_curves_ir_rates values ( 1, 2.2, 15000 );
-insert into currency_curves_ir_rates values ( 1, 2.2, 20000 );
+insert into currency_curves_ir_rates values ( 1, 0, 2.4, 10000 );
+insert into currency_curves_ir_rates values ( 1, 1, 2.5, 10010 );
+insert into currency_curves_ir_rates values ( 1, 2, 2.6, 10100 );
+insert into currency_curves_ir_rates values ( 1, 3, 2.5, 10200 );
+insert into currency_curves_ir_rates values ( 1, 4, 2.5, 10400 );
+insert into currency_curves_ir_rates values ( 1, 5, 2.4, 11000 );
+insert into currency_curves_ir_rates values ( 1, 6, 2.3, 12000 );
+insert into currency_curves_ir_rates values ( 1, 7, 2.3, 13000 );
+insert into currency_curves_ir_rates values ( 1, 8, 2.2, 15000 );
+insert into currency_curves_ir_rates values ( 1, 9, 2.2, 20000 );
 
