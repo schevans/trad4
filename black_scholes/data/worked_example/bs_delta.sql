a86d8fc07a0ce9b7713fbb920e23d9ffb1e349c4
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-05-19 17:20:31 +0000

    Fixed seemingly ancient bug.

--- black_scholes/data/worked_example/bs_delta.sql
+++ black_scholes/data/worked_example/bs_delta.sql
@@ -1,3 +1,3 @@
 delete from bs_delta;
-insert into object values ( 6, 6, 4, "bs_delta_6", 1, 1 );
+insert into object values ( 6, 6, 3, "bs_delta_6", 1, 1 );
 insert into bs_delta values ( 6, 5, 4, 1 );
