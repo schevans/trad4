5c88244b66f9625a9df89b1f50d4c3163fa54889
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-12-20 20:20:10 +0000

    Added support to write the weight_matrices as .png files. Currently dependant on a libgd install but I'll be adding that lib to the vt distribution. There's an early 16-bit example of the converged map for '1' here: http://trad4.sourceforge.net/vision_thing/neuron_1_48.png.

--- trunk/vision_thing/objects/monitor.c
+++ trunk/vision_thing/objects/monitor.c
@@ -5,8 +5,13 @@
 //  to see what's in-scope.
 
 #include <iostream>
+#include <map>
+
+#include "gd.h"
 
 #include "monitor_wrapper.c"
+
+void save_weight_matrix( weight_matrix* weight_matrix, string filename );
 
 using namespace std;
 
@@ -17,8 +22,6 @@ int calculate_monitor( obj_loc_t obj_loc, int id )
         monitor_num_runs = 0;
         monitor_num_cycles_correct = 0;
     }
-
-    int num_cucles_correct = 0;
 
     int local_all_correct = 1;
 
@@ -47,6 +50,107 @@ int calculate_monitor( obj_loc_t obj_loc, int id )
 
     monitor_num_runs++;
 
+    for ( int i = 0 ; i < NUM_NEURONS ; i++ )
+    {
+        std::ostringstream filename;
+        filename << object_name( ((t4::monitor*)obj_loc[id])->neurons[i]) << "_" << monitor_num_runs << ".png";
+
+        save_weight_matrix( &neurons_weights( i ), filename.str() );
+
+        filename.clear();
+    }
+
     return 1;
 }
+
+void save_weight_matrix( weight_matrix* weight_matrix, string filename )
+{
+    FILE *pngout = fopen( filename.c_str(), "wb");
+
+    gdImagePtr im = gdImageCreate(NUM_COLS, NUM_ROWS);
+
+    double max_weight = 0;
+    double min_weight = 0;
+
+    for ( int row = 0 ; row < NUM_ROWS ; row++ )
+    {
+        for ( int col = 0 ; col < NUM_COLS ; col++ )
+        {
+            if ( (*weight_matrix).row[row].col[col] > max_weight )
+                max_weight = (*weight_matrix).row[row].col[col];
+
+            if ( (*weight_matrix).row[row].col[col] < min_weight )
+                min_weight = (*weight_matrix).row[row].col[col];
+        }
+    }
+
+    double max_weight_coeff = 127/max_weight;
+    double min_weight_coeff = 127/min_weight;
+
+    //cout << "max_weight: " << max_weight << ", min_weight: " << min_weight << "max_weight_coeff: " << max_weight_coeff << ", min_weight_coeff: " << min_weight_coeff << endl;
+
+    int grey = gdImageColorAllocate(im, 127, 127, 127 ); 
+
+    std::map<double, int> colour_table;
+
+    double this_weight(0);
+
+    for ( int row = 0 ; row < NUM_ROWS ; row++ )
+    {
+        for ( int col = 0 ; col < NUM_COLS ; col++ )
+        {
+            this_weight = (*weight_matrix).row[row].col[col]; 
+
+            if ( this_weight > 0 )
+            {
+                if ( ! colour_table[this_weight] )
+                {
+
+                    int colour = 127 - ( this_weight * max_weight_coeff );
+
+                    colour_table[this_weight] = gdImageColorAllocate(im, colour, colour, colour ); 
+
+                   //cout << "Creating new colour for " << this_weight << ": " << colour << "("<< colour_table[this_weight]  <<")" <<endl;
+                }
+            } 
+            else if ( this_weight < 0 )
+            {
+                if ( ! colour_table[this_weight] )
+                {
+                    int colour = 127 - ( this_weight * -min_weight_coeff );
+
+                    colour_table[this_weight] = gdImageColorAllocate(im, colour, colour, colour ); 
+
+                   //cout << "Creating new colour for " << this_weight << ": " << colour << "("<< colour_table[this_weight]  <<")" <<endl;
+                }
+
+            }
+            else 
+            {
+                colour_table[this_weight] = grey;
+            }
+
+        }
+    }
+
+    int this_colour(0);
+
+    for ( int row = 0 ; row < NUM_ROWS ; row++ )
+    {
+        for ( int col = 0 ; col < NUM_COLS ; col++ )
+        {
+            this_weight = (*weight_matrix).row[row].col[col]; 
+            this_colour = colour_table[this_weight];
+
+            gdImageSetPixel( im, col, row, this_colour );
+        }
+    }
+
+    gdImagePng(im, pngout);
+
+    fclose(pngout);
+    gdImageDestroy(im);
+
+}
+
 
