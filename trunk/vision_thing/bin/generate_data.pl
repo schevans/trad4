2a055ecf05523d8857bed8ccf921038ae46cbc0c
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2009-12-21 20:21:47 +0000

    Added support for loading 64-bit .png files. The data in question was roughly culled from an open font called LiberationSerif.

--- trunk/vision_thing/bin/generate_data.pl
+++ trunk/vision_thing/bin/generate_data.pl
@@ -3,7 +3,7 @@
 use warnings;
 use strict;
 
-my $outfile = "/home/steve/src/vision_thing/data/16bit_numbers/gen.sql";
+my $outfile = "/home/steve/src/vision_thing/data/64bit_numbers/gen.sql";
 open OUTFILE, ">$outfile" or die "Can't open $outfile.\n";
 
 my $row;
@@ -20,9 +20,6 @@ print OUTFILE "BEGIN;\n";
 print OUTFILE "delete from object;\n";
 print OUTFILE "delete from input;\n";
 print OUTFILE "delete from neuron;\n";
-print OUTFILE "delete from input_images;\n";
-print OUTFILE "delete from input_images_row;\n";
-print OUTFILE "delete from input_images_row_col;\n";
 
 print OUTFILE "insert into object values ( $input_id, 1, 1, \"input\", 0, 1 );\n";
 print OUTFILE "insert into input values ( $input_id, $monitor_id );\n";
@@ -34,33 +31,8 @@ my @images = ( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 );
 
 for $image ( @images ) {
 
-    my $row_counter=0;
-
     my $infile = "/home/steve/src/vision_thing/data/16bit_numbers/$image.txt";
     open INFILE, $infile or die "Can't open $infile.\n";
-
-    print OUTFILE "insert into input_images values ( $input_id, $image );\n";
-
-    while ( $row = <INFILE> ) {
-
-        chomp( $row );
-
-        my @row_arr = split //, $row;
-        my $column_counter;
-
-        print OUTFILE "insert into input_images_row values ( $input_id, $image, $row_counter );\n";
-
-        print OUTFILE "insert into input_images_row_col values ( $input_id, $image, $row_counter";
-
-        for ( $column_counter = 0 ; $column_counter < $NUM_COLUMNS ; $column_counter++ ) {
-
-            print OUTFILE ", $row_arr[$column_counter]";
-        }
-
-        print OUTFILE ");\n";
-
-        $row_counter++
-    }
 
     $neuron_id = $image+1; 
 
