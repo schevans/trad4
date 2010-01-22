4691be8191eaedfd599e3636ba1935e198637866
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-01-22 19:32:19 +0000

    Fixed (probably harmless) out-of-bounds read in compression.

--- trunk/vision_thing/objects/monitor.c
+++ trunk/vision_thing/objects/monitor.c
@@ -286,7 +286,14 @@ void create_animation( obj_loc_t obj_loc, int id )
 
             gdImageCopy(frame_imgs[run_num], text_pane, png_frame_width, png_frame_height*2, 0, 0, png_frame_width*4, png_frame_height );
 
-            gdImageGifAnimAdd(frame_imgs[run_num], out, 1, 0, 0, 100, 1, frame_imgs[run_num-1]);
+            if ( run_num == 0 ) 
+            {
+                gdImageGifAnimAdd(frame_imgs[run_num], out, 1, 0, 0, 100, 1, NULL);
+            }
+            else 
+            {
+                gdImageGifAnimAdd(frame_imgs[run_num], out, 1, 0, 0, 100, 1, frame_imgs[run_num-1]);
+            }
 
             gdImageDestroy(text_pane);
             gdImageDestroy(input_image);
