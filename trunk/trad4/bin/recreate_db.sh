1490589e7080c3985797f656023ea73ee36cb260
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-03-22 21:01:24 +0000

    Removed deprecated reference to $APP_ROOT/sql.

--- trunk/trad4/bin/recreate_db.sh
+++ trunk/trad4/bin/recreate_db.sh
@@ -28,20 +28,4 @@ do
     $TRAD4_ROOT/bin/lsql $i
 done
 
-if [ -d $APP_ROOT/sql ] 
-then
-    echo "Loading all in $APP_ROOT/sql..."
-
-    cd $APP_ROOT/sql
-
-    for i in `ls *.table`
-    do
-        echo "Loading $i"
-
-        $TRAD4_ROOT/bin/lsql $i
-    done
-
-fi
-
-
 
