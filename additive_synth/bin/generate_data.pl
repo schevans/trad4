ce6eb2d1c7c7e1d70e18d01baaffe1e398bfc9a4
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-04-06 21:02:11 +0000

    Extended model to 4x4 harmonics.

--- additive_synth/bin/generate_data.pl
+++ additive_synth/bin/generate_data.pl
@@ -13,12 +13,14 @@ open OUTFILE, ">$outfile" or die "Can't open $outfile.\n";
 my $harmonic_base_id = 1;
 my $mixer_base_id = 1000;
 
-my $NUM_HARMONICS=4;
+my $NUM_HARMONICS=16;
+my $NUM_HARMONICS_PER_MIXER=4;
 
 print OUTFILE "BEGIN;\n";
 print OUTFILE "delete from object;\n";
 print OUTFILE "delete from harmonic;\n";
 print OUTFILE "delete from mixer;\n";
+print OUTFILE "delete from mixer_samples;\n";
 
 for ( my $i=0 ; $i < $NUM_HARMONICS ; $i++ )
 {
@@ -27,12 +29,12 @@ for ( my $i=0 ; $i < $NUM_HARMONICS ; $i++ )
     print OUTFILE "insert into object values ( $harmonic_id, 1, 1, \"harmonic_$harmonic_id\", 1, 1 );\n";
     print OUTFILE "insert into harmonic values ( $harmonic_id, 0 );\n";
 
-    print OUTFILE "insert into harmonic_samples values ( $harmonic_id, $harmonic_id, $harmonic_id );\n";
+    print OUTFILE "insert into harmonic_samples values ( $harmonic_id, $harmonic_id, $harmonic_id, $harmonic_id, $harmonic_id );\n";
 }
 
 my $num_mixers=0;
 
-for ( my $i=0 ; $i < $NUM_HARMONICS / 2 ; $i++ )
+for ( my $i=0 ; $i < $NUM_HARMONICS / $NUM_HARMONICS_PER_MIXER ; $i++ )
 {
     my $mixer_id = $mixer_base_id + $i;
 
@@ -40,19 +42,21 @@ for ( my $i=0 ; $i < $NUM_HARMONICS / 2 ; $i++ )
 
     print OUTFILE "insert into mixer values ( $mixer_id, 1 );\n";
 
-my $h1id = ( $i * 2 ) + 1;
-my $h2id = ( $i * 2 ) + 2;
+my $h1id = ( $i * $NUM_HARMONICS_PER_MIXER ) + 1;
+my $h2id = ( $i * $NUM_HARMONICS_PER_MIXER ) + 2;
+my $h3id = ( $i * $NUM_HARMONICS_PER_MIXER ) + 3;
+my $h4id = ( $i * $NUM_HARMONICS_PER_MIXER ) + 4;
 
-print "M $mixer_id $h1id $h2id\n";
+print "M $i $mixer_id $h1id $h2id $h3id $h4id\n";
 
-    print OUTFILE "insert into mixer_samples values ( $mixer_id, $h1id, $h2id );\n";
+    print OUTFILE "insert into mixer_samples values ( $mixer_id, $h1id, $h2id, $h3id, $h4id );\n";
 }
 
-print OUTFILE "insert into object values ( 1002, 2, 3, \"mixer_1002\", 1, 1 );\n";
+print OUTFILE "insert into object values ( 1004, 2, 3, \"master\", 1, 1 );\n";
 
-print OUTFILE "insert into mixer values ( 1002, 1 );\n";
+print OUTFILE "insert into mixer values ( 1004, 1 );\n";
 
-print OUTFILE "insert into mixer_samples values ( 1002, 1000, 1001 );\n";
+print OUTFILE "insert into mixer_samples values ( 1004, 1000, 1001, 1002, 1003 );\n";
 
 print OUTFILE "COMMIT;\n";
 
