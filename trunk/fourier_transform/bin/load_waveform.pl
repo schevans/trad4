d6548b05d426fb76d29bb81079dc2f411882e58d
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-06-11 15:10:47 +0000

    Added load_waveform.pl.

---
+++ trunk/fourier_transform/bin/load_waveform.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+# Copyright (c) Steve Evans 2010
+# steve@topaz.myzen.co.uk
+# This code is licenced under the BSD licence. For details see $INSTANCE_ROOT/LICENCE
+
+use warnings;
+use strict;
+
+use Data::Dumper;
+
+use PreComp::AppConstants;
+use PreComp::Utilities;
+
+if ( $#ARGV != 1 ) {
+
+    print "Usage: load_waveform.pl <filename> <frequency>\n";
+    exit(1);
+}
+
+my $input_filename=$ARGV[0];
+my $base_frequency=$ARGV[1];
+
+if ( ! -f $input_filename ) {
+
+    print "Error: Can't find $input_filename.\n";
+    exit(1);
+}
+
+`cp $input_filename $ENV{APP_ROOT}/source.wav`;
+
+`echo "update source set base_frequency=$base_frequency;" | $ENV{SQLITE} $ENV{APP_DB}`;
+
+my $is_running = `ps -C $ENV{APP} | grep -v PID | wc -l`;
+
+chomp( $is_running );
+
+if ( $is_running ) {
+
+    print "Sending reload signal.\n";
+
+    `$ENV{TRAD4_BIN}/send_reload.sh`;
+}
+
