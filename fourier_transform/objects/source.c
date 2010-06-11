59d17dcc999ead2833e8c729b17645b5d76f0c9d
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-06-11 15:10:27 +0000

    Fixed a reload bug.

--- fourier_transform/objects/source.c
+++ fourier_transform/objects/source.c
@@ -14,31 +14,28 @@ using namespace std;
 
 int calculate_source( obj_loc_t obj_loc, int id )
 {
-    if ( ! object_init(id ) )
+    SNDFILE *file;
+
+    SF_INFO sfinfo;
+
+    memset (&sfinfo, 0, sizeof (sfinfo));
+
+    sfinfo.format = 0;
+
+    string infile = "./source.wav";
+
+    if (! (file = sf_open ( infile.c_str(), SFM_READ, &sfinfo)))
     {
-        SNDFILE *file;
-
-        SF_INFO sfinfo;
-
-        memset (&sfinfo, 0, sizeof (sfinfo));
-
-        sfinfo.format = 0;
-
-        string infile = "./source.wav";
-
-        if (! (file = sf_open ( infile.c_str(), SFM_READ, &sfinfo)))
-        {
-            printf ("Error : Not able to open input file %s.\n", infile.c_str());
-            exit(0);
-        }
-
-        if (sf_read_int (file, source_wave, SAMPLE_COUNT) != SAMPLE_COUNT)
-        {
-            puts (sf_strerror (file));
-        }
-
-        sf_close(file);
+        printf ("Error : Not able to open input file %s.\n", infile.c_str());
+        exit(0);
     }
+
+    if (sf_read_int (file, source_wave, SAMPLE_COUNT) != SAMPLE_COUNT)
+    {
+        puts (sf_strerror (file));
+    }
+
+    sf_close(file);
 
     return 1;
 }
