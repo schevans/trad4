f96938676a3eb83d6a34a93b6caa40e28bd1a8ec
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-08-13 20:06:17 +0000

    Renamed currency_curves => ir_curve and temporarily disabled repo_trade and fx_rate.

---
+++ bond_risk/objects/ir_curve.c
@@ -0,0 +1,66 @@
+// Copyright (c) Steve Evans 2008
+// steve@topaz.myzen.co.uk
+//
+
+#include <iostream>
+#include <math.h>
+
+#include "ir_curve_wrapper.c"
+
+using namespace std;
+
+int calculate_ir_curve( obj_loc_t obj_loc, int id )
+{
+    int current_period_start;
+    int current_period_end;
+
+    double gradient;
+    double y_intercept;
+
+    // Interpolate the IR first..
+    for ( int indx = 0; indx < INTEREST_RATE_LEN - 1 ; indx++)
+    {
+        current_period_start = ir_curve_input_rates_asof(indx);
+        current_period_end = ir_curve_input_rates_asof(indx+1);
+
+        gradient = ((  ir_curve_input_rates_value(indx) -  ir_curve_input_rates_value(indx+1) ) / (  ir_curve_input_rates_asof(indx) -  ir_curve_input_rates_asof(indx+1) ) );
+        y_intercept = ir_curve_input_rates_value(indx) - gradient * ir_curve_input_rates_asof(indx);
+
+/*
+        cout << "current_period_start: " << current_period_start 
+            << ", current_period_end: " << current_period_end 
+            << ", ir_curve_input_rates_value(indx): " << ir_curve_input_rates_value(indx)
+            << ", ir_curve_input_rates_value(indx+1): " << ir_curve_input_rates_value(indx+1)
+            << ", gradient: " << gradient 
+            << ", y_intercept: " << y_intercept << endl;
+*/
+        for ( int i = current_period_start ; i < current_period_end ; i++ )
+        {
+            //cout << "\tDate " << i << " index  " << i - DATE_RANGE_START << " rate: " <<(  i*gradient + y_intercept ) << endl;
+            ir_curve_interest_rate_interpol[i - DATE_RANGE_START] = (  i*gradient + y_intercept );
+        }
+
+    }
+    
+    // Then calc the discount rates..
+
+    for ( int i = 0 ; i <= DATE_RANGE_LEN ; i++ )
+    {
+        ir_curve_discount_rate[i] = exp( -ir_curve_interest_rate_interpol[i] * (( i / YEAR_BASIS)/ YEAR_BASIS ) );
+
+        ir_curve_discount_rate_p01[i] = exp( -(ir_curve_interest_rate_interpol[i] +0.0001) * (( i / YEAR_BASIS)/ YEAR_BASIS ) );
+
+        ir_curve_discount_rate_m01[i] = exp( -(ir_curve_interest_rate_interpol[i] -0.0001) * (( i / YEAR_BASIS)/ YEAR_BASIS ) );
+
+/*
+        cout << "ir_curve_discount_rate[ " << i << "]: " << ir_curve_discount_rate[i]
+            << ", ir_curve_discount_rate_p01[ " << i << "]: " << ir_curve_discount_rate_p01[i]
+            << ", ir_curve_discount_rate_m01[ " << i << "]: " << ir_curve_discount_rate_m01[i]
+            << endl;
+*/
+    }
+
+
+    return 1;
+}
+
