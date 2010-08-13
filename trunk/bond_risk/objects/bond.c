fb205c8de6767947e5ea2627f37ba105a37435d4
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-08-13 20:06:17 +0000

    Renamed currency_curves => ir_curve and temporarily disabled repo_trade and fx_rate.

--- trunk/bond_risk/objects/bond.c
+++ trunk/bond_risk/objects/bond.c
@@ -12,10 +12,10 @@ using namespace std;
 
 int calculate_bond( obj_loc_t obj_loc, int id )
 {
-    std::vector<int> _coupon_date_vec;
-    std::vector<float> _payment_vec;
-    std::vector<float> _payment_vec_p01;
-    std::vector<float> _payment_vec_m01;
+    std::vector<int> coupon_date_vec;
+    std::vector<float> payment_vec;
+    std::vector<float> payment_vec_p01;
+    std::vector<float> payment_vec_m01;
 
     int days_between_coupons = (int)YEAR_BASIS / bond_coupons_per_year;
     float coupon_discount_rate_per_period = ( bond_coupon / 100 ) / bond_coupons_per_year;
@@ -29,35 +29,34 @@ int calculate_bond( obj_loc_t obj_loc, int id )
 
     while ( working_date < bond_maturity_date  )
     {
-        _coupon_date_vec.push_back( working_date );
-
+        coupon_date_vec.push_back( working_date );
         working_date = working_date + days_between_coupons;
     }
-    _coupon_date_vec.push_back( working_date );
+    coupon_date_vec.push_back( working_date );
 
     vector<int>::iterator iter;
 
-    for( iter = _coupon_date_vec.begin() ; iter < _coupon_date_vec.end() ; iter++ )
+    for( iter = coupon_date_vec.begin() ; iter < coupon_date_vec.end() ; iter++ )
     {
-        _payment_vec.push_back( 100.0 * coupon_discount_rate_per_period * currency_curves_discount_rate[*iter - TODAY] );
-        _payment_vec_p01.push_back( 100.0 * coupon_discount_rate_per_period * currency_curves_discount_rate_p01[*iter - TODAY] );
-        _payment_vec_m01.push_back( 100.0 * coupon_discount_rate_per_period * currency_curves_discount_rate_m01[*iter - TODAY] );
+        payment_vec.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate[*iter - TODAY] );
+        payment_vec_p01.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate_p01[*iter - TODAY] );
+        payment_vec_m01.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate_m01[*iter - TODAY] );
     }
 
-    _payment_vec.push_back( 100 * currency_curves_discount_rate[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
-    _payment_vec_p01.push_back( 100 * currency_curves_discount_rate_p01[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
-    _payment_vec_m01.push_back( 100 * currency_curves_discount_rate_m01[_coupon_date_vec[_coupon_date_vec.size() -1] - TODAY] );
+    payment_vec.push_back( 100 * ir_curve_discount_rate[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
+    payment_vec_p01.push_back( 100 * ir_curve_discount_rate_p01[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
+    payment_vec_m01.push_back( 100 * ir_curve_discount_rate_m01[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
 
     float price(0.0);
     float price_p01(0.0);
     float price_m01(0.0);
 
-    for( unsigned int i = 0 ; i < _payment_vec.size() ; i++ )
+    for( unsigned int i = 0 ; i < payment_vec.size() ; i++ )
     {
-        price = price + _payment_vec[i];
+        price = price + payment_vec[i];
 
-        price_p01 = price_p01 + _payment_vec_p01[i];
-        price_m01 = price_m01 + _payment_vec_m01[i];
+        price_p01 = price_p01 + payment_vec_p01[i];
+        price_m01 = price_m01 + payment_vec_m01[i];
 
     }
 
