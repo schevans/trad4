6382e0c73b6ddc70c5390a0f5d10b7256670f833
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-08-18 20:26:47 +0000

    Renamed currency_curves => ir_curve and temporarily disabled repo_trade and fx_rate.

--- trunk/bond_risk/data/worked_example/currency_curves.sql
+++
@@ -1,3 +0,0 @@
-delete from currency_curves;
-insert into object values ( 1, 1, 1, "LIBOR-USD", 0, 1 ); 
-insert into currency_curves values ( 1 );
---
+++ trunk/bond_risk/data/worked_example/ir_curve_input_rates.sql
@@ -0,0 +1,12 @@
+delete from ir_curve_input_rates;
+insert into ir_curve_input_rates values ( 1, 0, 2.4, 10000 );
+insert into ir_curve_input_rates values ( 1, 1, 2.5, 10010 );
+insert into ir_curve_input_rates values ( 1, 2, 2.6, 10100 );
+insert into ir_curve_input_rates values ( 1, 3, 2.5, 10200 );
+insert into ir_curve_input_rates values ( 1, 4, 2.5, 10400 );
+insert into ir_curve_input_rates values ( 1, 5, 2.4, 11000 );
+insert into ir_curve_input_rates values ( 1, 6, 2.3, 12000 );
+insert into ir_curve_input_rates values ( 1, 7, 2.3, 13000 );
+insert into ir_curve_input_rates values ( 1, 8, 2.2, 15000 );
+insert into ir_curve_input_rates values ( 1, 9, 2.2, 20000 );
+
