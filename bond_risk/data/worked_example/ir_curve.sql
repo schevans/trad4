cb881a4b45ebe7f6d8110e0a7ce22e68347670b8
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-08-18 20:26:47 +0000

    Renamed currency_curves => ir_curve and temporarily disabled repo_trade and fx_rate.

---
+++ bond_risk/data/worked_example/ir_curve.sql
@@ -0,0 +1,3 @@
+delete from ir_curve;
+insert into object values ( 1, 1, 1, "LIBOR-USD", 0, 1 ); 
+insert into ir_curve values ( 1 );
--- bond_risk/data/worked_example/outright_trade.sql
+++ bond_risk/data/worked_example/outright_trade.sql
@@ -1,4 +1,4 @@
 delete from outright_trade;
-insert into object values ( 4, 4, 3, "O38273", 0, 1 );
-insert into outright_trade values ( 4, 102.8, 0, 10000, 9982, 3 );
+insert into object values ( 3, 3, 3, "O38273", 0, 1 );
+insert into outright_trade values ( 3, 102.8, 0, 10000, 9982, 2 );
 
