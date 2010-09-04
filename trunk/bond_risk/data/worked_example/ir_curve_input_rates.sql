7ebb7e245b8fd3c9a0739db476cc43df9a038407
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-04 09:11:13 +0000

    Updated to use new dates.

--- trunk/bond_risk/data/worked_example/ir_curve_input_rates.sql
+++ trunk/bond_risk/data/worked_example/ir_curve_input_rates.sql
@@ -1,12 +1,12 @@
 delete from ir_curve_input_rates;
-insert into ir_curve_input_rates values ( 1, 0, 2.4, 10000 );
-insert into ir_curve_input_rates values ( 1, 1, 2.5, 10010 );
-insert into ir_curve_input_rates values ( 1, 2, 2.6, 10100 );
-insert into ir_curve_input_rates values ( 1, 3, 2.5, 10200 );
-insert into ir_curve_input_rates values ( 1, 4, 2.5, 10400 );
-insert into ir_curve_input_rates values ( 1, 5, 2.4, 11000 );
-insert into ir_curve_input_rates values ( 1, 6, 2.3, 12000 );
-insert into ir_curve_input_rates values ( 1, 7, 2.3, 13000 );
-insert into ir_curve_input_rates values ( 1, 8, 2.2, 15000 );
-insert into ir_curve_input_rates values ( 1, 9, 2.2, 20000 );
+insert into ir_curve_input_rates values ( 1, 0, 2.4, 40403 );
+insert into ir_curve_input_rates values ( 1, 1, 2.5, 40590 );
+insert into ir_curve_input_rates values ( 1, 2, 2.6, 40773 );
+insert into ir_curve_input_rates values ( 1, 3, 2.5, 41138 );
+insert into ir_curve_input_rates values ( 1, 4, 2.5, 41868 );
+insert into ir_curve_input_rates values ( 1, 5, 2.4, 42598 );
+insert into ir_curve_input_rates values ( 1, 6, 2.3, 43328 );
+insert into ir_curve_input_rates values ( 1, 7, 2.3, 44058 );
+insert into ir_curve_input_rates values ( 1, 8, 2.2, 45883 );
+insert into ir_curve_input_rates values ( 1, 9, 2.2, 47708 );
 
