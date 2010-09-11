4ebdde6374a2217dc3e744a12fd8eaa0e7261235
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-11 12:53:51 +0000

    Spruced-up bond.

--- trunk/bond_risk/data/worked_example/bond.sql
+++ trunk/bond_risk/data/worked_example/bond.sql
@@ -1,4 +1,4 @@
 delete from bond;
 insert into object values ( 2, 2, 2, "US81823453", 0, 1 );
-insert into bond values ( 2, 15000, 5000, 1, 2.8, 1 );
+insert into bond values ( 2, 39814, 43467, 1, 0.028, 1 );
 
