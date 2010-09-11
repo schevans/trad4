fbb8de9ffb6bc920d5b063d08d05fa63db759de0
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-11 12:53:51 +0000

    Spruced-up bond.

--- bond_risk/objects/bond.c
+++ bond_risk/objects/bond.c
@@ -12,55 +12,59 @@ using namespace std;
 
 int calculate_bond( obj_loc_t obj_loc, int id )
 {
-    std::vector<int> coupon_date_vec;
-    std::vector<float> payment_vec;
-    std::vector<float> payment_vec_p01;
-    std::vector<float> payment_vec_m01;
+    // Generate the coupon schedule - the dates of the coupon payments.
+    // This is a bit fiddly, as always.
 
-    int days_between_coupons = (int)YEAR_BASIS / bond_coupons_per_year;
-    float coupon_discount_rate_per_period = ( bond_coupon / 100 ) / bond_coupons_per_year;
+    double days_between_coupons = YEAR_BASIS / bond_coupons_per_year;
+    double coupon_rate_per_period = bond_coupon / bond_coupons_per_year;
 
-    int working_date = bond_start_date;
+    std::vector<date> coupon_schedule;
+    coupon_schedule.push_back( bond_start_date );
 
-    while ( working_date < TODAY )
+    double coupon_date = bond_start_date;
+
+    while( coupon_date < bond_maturity_date - days_between_coupons )
     {
-        working_date = working_date + days_between_coupons;
+        coupon_date = coupon_date + days_between_coupons;
+        coupon_schedule.push_back((date)floor(coupon_date+0.5));
+    } 
+
+    // The schedule is now generated. Next we iterate over it, discounting the future coupon payments
+    // and summing the result.
+    // We need to do this for price and pv01.
+    
+    double price(0);
+    double price_p01(0);
+    double price_m01(0);
+    
+    std::vector<date>::const_iterator iter = coupon_schedule.begin();
+    
+    for ( ; iter != coupon_schedule.end() ; iter++ )
+    {
+        if ( *iter >= TODAY )   // Only consider payments today and in the future.
+        {
+            // Calculate the offset we need to index into ir_curve
+            int ir_offset = (*iter) - TODAY;
+
+            // Get the discount_rate from ir_curve 
+            double discount_rate = ir_curve_discount_rate[ir_offset];
+
+            // Use the discount_rate to discount the future coupon payments
+            price = price + ( 100.0 * coupon_rate_per_period * discount_rate );
+
+            // And calculate the price _p01 and _m01, used to calculate the pv01.
+            price_p01 = price_p01 + ( 100.0 * coupon_rate_per_period * ir_curve_discount_rate_p01[ir_offset] );
+            price_m01 = price_m01 + ( 100.0 * coupon_rate_per_period * ir_curve_discount_rate_m01[ir_offset] );
+        }
     }
 
-    while ( working_date < bond_maturity_date  )
-    {
-        coupon_date_vec.push_back( working_date );
-        working_date = working_date + days_between_coupons;
-    }
-    coupon_date_vec.push_back( working_date );
+    // Add in the principal repayment at maturity
+    price = price + ( 100.0 * ir_curve_discount_rate[bond_maturity_date - TODAY] );
+    price_p01 = price_p01 + ( 100.0 * ir_curve_discount_rate_p01[bond_maturity_date - TODAY] );
+    price_m01 = price_m01 + ( 100.0 * ir_curve_discount_rate_m01[bond_maturity_date - TODAY] );
 
-    vector<int>::iterator iter;
-
-    for( iter = coupon_date_vec.begin() ; iter < coupon_date_vec.end() ; iter++ )
-    {
-        payment_vec.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate[*iter - TODAY] );
-        payment_vec_p01.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate_p01[*iter - TODAY] );
-        payment_vec_m01.push_back( 100.0 * coupon_discount_rate_per_period * ir_curve_discount_rate_m01[*iter - TODAY] );
-    }
-
-    payment_vec.push_back( 100 * ir_curve_discount_rate[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
-    payment_vec_p01.push_back( 100 * ir_curve_discount_rate_p01[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
-    payment_vec_m01.push_back( 100 * ir_curve_discount_rate_m01[coupon_date_vec[coupon_date_vec.size() -1] - TODAY] );
-
-    float price(0.0);
-    float price_p01(0.0);
-    float price_m01(0.0);
-
-    for( unsigned int i = 0 ; i < payment_vec.size() ; i++ )
-    {
-        price = price + payment_vec[i];
-
-        price_p01 = price_p01 + payment_vec_p01[i];
-        price_m01 = price_m01 + payment_vec_m01[i];
-
-    }
-
-    bond_price = ( price );
+    // And assign the results
+    bond_price = price;
     bond_pv01 = ( price_p01 - price_m01 ) / 2.0;
 
     return 1;
