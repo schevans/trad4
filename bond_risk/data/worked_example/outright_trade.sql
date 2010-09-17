906a7e4086a2b019546eb5377dfd24f9779c0a32
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-17 10:40:14 +0000

    Spruced-up outright_trade.

--- bond_risk/data/worked_example/outright_trade.sql
+++ bond_risk/data/worked_example/outright_trade.sql
@@ -1,4 +1,4 @@
 delete from outright_trade;
 insert into object values ( 3, 3, 3, "O38273", 0, 1 );
-insert into outright_trade values ( 3, 102.8, 0, 10000, 9982, 2 );
+insert into outright_trade values ( 3, 104.7, 0, 1000, 40323, 2 );
 
