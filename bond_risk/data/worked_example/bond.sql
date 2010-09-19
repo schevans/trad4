2922557ef5ef88480f14f22906dfe2410bb6894a
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-19 10:38:28 +0000

    Spruced-up fx_rate and repo_trade. Also upped log_level.

--- bond_risk/data/worked_example/bond.sql
+++ bond_risk/data/worked_example/bond.sql
@@ -1,4 +1,4 @@
 delete from bond;
-insert into object values ( 2, 2, 2, "US81823453", 0, 1 );
+insert into object values ( 2, 2, 2, "US81823453", 2, 1 );
 insert into bond values ( 2, 39814, 43467, 1, 0.028, 1 );
 
--- bond_risk/data/worked_example/outright_trade.sql
+++ bond_risk/data/worked_example/outright_trade.sql
@@ -1,4 +1,4 @@
 delete from outright_trade;
-insert into object values ( 3, 3, 3, "O38273", 0, 1 );
-insert into outright_trade values ( 3, 104.7, 0, 1000, 40323, 2 );
+insert into object values ( 3, 3, 3, "O38273", 2, 1 );
+insert into outright_trade values ( 3, 104.7, 2, 1000, 40323, 2 );
 
