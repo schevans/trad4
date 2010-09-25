5583ecf2b818d3c271456d3ff26f75eb54d33507
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-09-25 17:27:26 +0000

    Tweaked data in tier1_double_array.

--- trunk/test_app/objects/tier3.c
+++ trunk/test_app/objects/tier3.c
@@ -14,7 +14,7 @@ int calculate_tier3( obj_loc_t obj_loc, int id )
 {
     T4_TEST( my_tier2_p_int, 14965 );
     T4_TEST( my_tier1_int_out - my_tier2_p_int, 38 );
-    T4_TEST( my_tier1_double_array[ 2 ] * my_tier1_double_array[ 3 ], 12.9996 );
+    T4_TEST( my_tier1_double_array[ 2 ] * my_tier1_double_array[ 3 ], 6.7196 );
 
     return 1;
 }
