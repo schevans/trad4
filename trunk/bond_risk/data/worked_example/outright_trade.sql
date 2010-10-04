055be0628281b64637a052fb62aacfa3e829a27e
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-10-04 17:43:52 +0000

    Removed fx_rate. Re-jigged layout again.

--- trunk/bond_risk/data/worked_example/outright_trade.sql
+++ trunk/bond_risk/data/worked_example/outright_trade.sql
@@ -1,4 +1,4 @@
 delete from outright_trade;
-insert into object values ( 5, 5, 3, "O38273", 1, 1 );
-insert into outright_trade values ( 5, 104.7, 2, 1000, 40323, 4 );
+insert into object values ( 4, 4, 3, "O38273", 1, 1 );
+insert into outright_trade values ( 4, 104.7, 2, 1000, 40323, 3 );
 
