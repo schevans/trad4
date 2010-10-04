4a7422cb9412cbbeb9e485597b41a55b5ffab4b8
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-10-04 17:43:52 +0000

    Removed fx_rate. Re-jigged layout again.

--- bond_risk/data/worked_example/bond.sql
+++ bond_risk/data/worked_example/bond.sql
@@ -1,4 +1,4 @@
 delete from bond;
-insert into object values ( 4, 4, 2, "US81823453", 1, 1 );
-insert into bond values ( 4, 39814, 43467, 1, 0.028, 1, 3 );
+insert into object values ( 3, 3, 2, "US81823453", 1, 1 );
+insert into bond values ( 3, 39814, 43467, 1, 0.028, 1, 2 );
 
--- bond_risk/data/worked_example/fx_rate.sql
+++
@@ -1,3 +0,0 @@
-delete from fx_rate;
-insert into object values ( 2, 2, 1, "GBP-USD", 1, 1 );
-insert into fx_rate values ( 2, 1.6941, 2, 1 );
