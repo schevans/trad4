9fa6c1ab231cdd17c1f38b7b1948bb968f670755
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-11-01 20:40:35 +0000

    Removed book from the trades - this needn't be a trade property.

--- trunk/bond_risk/data/worked_example/repo_trade.sql
+++ trunk/bond_risk/data/worked_example/repo_trade.sql
@@ -1,4 +1,4 @@
 delete from repo_trade;
 insert into object values ( 5, 5, 3, "R25745", 1, 1 );
-insert into repo_trade values ( 5, 40331, 40696, 10000, 1048700, 0.0425, 1, 1, 0, 1, 3, 2 );
+insert into repo_trade values ( 5, 40331, 40696, 10000, 1048700, 0.0425, 1, 0, 1, 3, 2 );
 
