6025a84dcbb4b00a335d99743bae785ebeaf6eb3
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2010-11-03 19:35:41 +0000

    Fixing up generate_data.pl.

--- bond_risk/bin/generate_data.pl
+++ bond_risk/bin/generate_data.pl
@@ -17,16 +17,17 @@ sub generate_ir_curve_input_rates();
 sub generate_bonds($);
 sub generate_outright_trades($);
 sub generate_repo_trades($);
+sub generate_outright_books();
+sub generate_repo_books();
 
 my $current_id = 201;
 my $log_level = 1;
 
 my %discount_rate_ids;
-my %bond_ccys;
-my %fx_rates;
 my @outright_agg;
 my @repo_agg;
 my @ir_curve_ids;
+my %ir_curve_name;
 
 my @coup_pys = ( 1, 2, 4 );
 my @currencies = ( 1, 2, 3 );
@@ -46,12 +47,12 @@ if (  -f $ENV{SRC_DIR}."/constants.t4s" ) {
 }
 
 my $NUM_FORWARD_DAYS = $constants_hash->{NUM_FORWARD_DAYS};
+my $MAX_TRADES_PER_BOOK = $constants_hash->{MAX_TRADES_PER_BOOK};
 my $TODAY = 40403;
 
 my $data_root=$ARGV[0];
 my $outright_trades_file=$data_root."/outright_trades.sql";
 my $repo_trades_file=$data_root."/repo_trades.sql";
-my $fx_rate_file=$data_root."/fx_rates.sql";
 my $discount_rate_file=$data_root."/discount_rate.sql";
 my $interest_rate_file=$data_root."/interest_rate.sql";
 
@@ -61,7 +62,9 @@ if ( ! -d $data_root ) {
 }
 
 my @bond_ids;
-my %bond_to_ccy_curve;
+my %bond_to_ir_curve;
+my %outright_trade_to_bond;
+my %repo_trade_to_bond;
 
 generate_calendar();
 generate_ir_curve();
@@ -70,7 +73,9 @@ generate_ir_curve_input_rates();
 # small_set
 generate_bonds( 5 );
 generate_outright_trades( 9 );
-#generate_repo_trades( 100000 );
+generate_repo_trades( 7 );
+generate_outright_books();
+generate_repo_books();
 
 # 220k
 #generate_bonds( 20000 );
@@ -146,6 +151,7 @@ sub generate_ir_curve()
     print FILE "insert into ir_curve values ( $id );\n";
 
     push @ir_curve_ids, $id;
+    $ir_curve_name{$id} = $name;
 
     $id++;
     $name = "LIBOR.GBP";
@@ -154,6 +160,7 @@ sub generate_ir_curve()
     print FILE "insert into ir_curve values ( $id );\n";
 
     push @ir_curve_ids, $id;
+    $ir_curve_name{$id} = $name;
 
 print FILE "COMMIT;\n";
 return;
@@ -165,6 +172,7 @@ return;
     print FILE "insert into ir_curve values ( $id );\n";
 
     push @ir_curve_ids, $id;
+    $ir_curve_name{$id} = $name;
 
     print FILE "COMMIT;\n";
 
@@ -205,8 +213,7 @@ sub generate_bonds($) {
             $ir_curve.");\n";
             
         push @bond_ids, $id;
-        $bond_to_ccy_curve{$id} = $ir_curve;
-        $bond_ccys{$id} = $ir_curve;
+        $bond_to_ir_curve{$id} = $ir_curve;
     }
     print FILE "COMMIT;\n";
 
@@ -224,23 +231,26 @@ sub generate_outright_trades($) {
 
     print FILE "delete from outright_trade;\n";
 
-    my ( $id, $name, $ir_curve );
+    my ( $id, $name, $bond, $trade_date, $buy_sell );
 
     for ( my $i = 0 ; $i < $num_outrignt_trades ; $i++ ) {
 
         $id = get_next_id();
-
         $name = "0".int( rand(999999));
+        $bond = $bond_ids[ int( rand( @bond_ids )) ]; 
+        $trade_date = int( $TODAY - rand( 365*5  ));
+        $buy_sell = int(rand(2));
 
         print FILE "insert into object values (  $id, 4, 3, \"$name\", $log_level, 1 );\n";
 
         print FILE "insert into outright_trade values ( $id, ".
             sprintf("%.2f", 95.0 + (rand( 10 ))).", ".
             1000 * int( rand( 1000  )).", ".
-            int( 8000 + rand(  99999 - 8000 )).", ".
-            "0,".
-            $bond_ids[ int( rand( @bond_ids )) ].");\n";
+            $trade_date.", ".
+            "$buy_sell,".
+            $bond.");\n";
 
+        $outright_trade_to_bond{$id} = $bond;
     }
 
     print FILE "COMMIT;\n";
@@ -257,7 +267,6 @@ sub generate_repo_trades($) {
 
     print FILE "BEGIN;\n";
 
-
     print FILE "delete from repo_trade;\n";
 
     my ( $id, $name, $ir_curve );
@@ -265,34 +274,22 @@ sub generate_repo_trades($) {
     for ( my $i = 0 ; $i < $num_objects ; $i++ ) {
 
         $id = get_next_id();
-
         my $name = "R".int( rand(999999));
 
         print FILE "insert into object values (  $id, 5, 3, \"$name\", $log_level, 1 );\n";
 
-        my $cash_ccy = $currencies[ int( rand( @currencies )) ];
-        my $notional = 10000 * int( rand( 100  ));
+        my $start_date = int( $TODAY - rand( 365*5  ));
+        my $end_date = $start_date + 365*5;
+        my $notional = 1000 * int( rand( 100  ));
+        my $start_cash = $notional * int( sprintf("%.2f", 95.0 + (rand( 10 ))));
+        my $rate = sprintf("%.2f", (rand( 8 - 2 ) + 2));
+        my $buy_sell = int(rand(2));
         my $bond = $bond_ids[ int( rand( @bond_ids )) ];
-        my $bond_ccy = $bond_ccys{$bond};
-        my $interest_rate = $bond_to_ccy_curve{$bond};
+        my $ir_curve = $bond_to_ir_curve{$bond};
 
-        my $fx_rate_key = "$cash_ccy"."$bond_ccy"; 
-        my $fx_rate = $fx_rates{$fx_rate_key}; 
+        print FILE "insert into repo_trade values ( $id, $start_date, $end_date, $notional, $start_cash, $rate, $buy_sell, 1, $bond, $ir_curve );\n";
 
-        print FILE "insert into repo_trade values ( $id, ". 
-            sprintf("%.2f", (rand( 8 - 2 ) + 2)).",".
-            "$cash_ccy,".
-            int( 16000 + rand(  18000 - 16000 )).",".
-            "0,".
-            int( 8000 + rand(  99999 - 8000 )).",".
-            $notional * int( sprintf("%.2f", 95.0 + (rand( 10 )))).",".
-            "$notional,".
-            int(rand(2)).",".
-            $bond.",".
-            $interest_rate.",".
-            $fx_rate.
-            ");\n";
-
+        $repo_trade_to_bond{$id} = $bond;
     }
 
     print FILE "COMMIT;\n";
@@ -300,6 +297,149 @@ sub generate_repo_trades($) {
     close FILE;
 }
 
+sub generate_outright_books() {
+
+    my $file = "$data_root/outright_book.sql";
+    open FILE, ">$file" or die "Can't open $file";
+
+    print FILE "BEGIN;\n";
+
+    print FILE "delete from outright_book;\n";
+    print FILE "delete from outright_book_outright_trades;\n";
+
+    my $ir_curve;
+
+    foreach $ir_curve ( @ir_curve_ids ) {
+
+        my $book_number = 1;
+        my $id = get_next_id();
+        my $name = "OUTRIGHT_".$ir_curve_name{$ir_curve}."_BK$book_number";
+        my @trades;
+
+        print FILE "insert into object values (  $id, 6, 4, \"$name\", $log_level, 1 );\n";
+        print FILE "insert into outright_book values ( $id );";
+        print FILE "insert into outright_book_outright_trades values (  $id";
+
+        my $bond;
+
+        foreach $bond ( @bond_ids ) {
+
+            if ( $bond_to_ir_curve{$bond} == $ir_curve ) {
+
+                my $outright_trade;
+
+                foreach $outright_trade ( keys( %outright_trade_to_bond )) {
+
+                    if ( $outright_trade_to_bond{$outright_trade} == $bond ) {
+
+                        push @trades, $outright_trade;
+                        print FILE ", $outright_trade";
+
+                        if ( scalar( @trades ) >= $MAX_TRADES_PER_BOOK ) {
+
+                            print FILE " );\n";
+                           
+                            $book_number++; 
+                            $id = get_next_id();
+                            $name = "OUTRIGHT_".$ir_curve_name{$ir_curve}."_BK$book_number";
+                            @trades = [];
+
+                            print FILE "insert into object values ( $id, 6, 4, \"$name\", $log_level, 1 );\n";
+                            print FILE "insert into outright_book values ( $id );";
+                            print FILE "insert into outright_book_outright_trades values ( $id";
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        while ( scalar( @trades ) < $MAX_TRADES_PER_BOOK ) {
+
+            push @trades, 0;
+            print FILE ", 0";
+        }
+        
+        print FILE " );\n";
+    }
+
+    print FILE "COMMIT;\n";
+
+    close FILE;
+}
+
+sub generate_repo_books() {
+
+    my $file = "$data_root/repo_book.sql";
+    open FILE, ">$file" or die "Can't open $file";
+
+    print FILE "BEGIN;\n";
+
+    print FILE "delete from repo_book;\n";
+    print FILE "delete from repo_book_repo_trades;\n";
+
+    my $ir_curve;
+
+    foreach $ir_curve ( @ir_curve_ids ) {
+
+        my $book_number = 1;
+        my $id = get_next_id();
+        my $name = "REPO_".$ir_curve_name{$ir_curve}."_BK$book_number";
+        my @trades;
+
+        print FILE "insert into object values (  $id, 7, 4, \"$name\", $log_level, 1 );\n";
+        print FILE "insert into repo_book values ( $id );";
+        print FILE "insert into repo_book_repo_trades values (  $id";
+
+        my $bond;
+
+        foreach $bond ( @bond_ids ) {
+
+            if ( $bond_to_ir_curve{$bond} == $ir_curve ) {
+
+                my $repo_trade;
+
+                foreach $repo_trade ( keys( %repo_trade_to_bond )) {
+
+                    if ( $repo_trade_to_bond{$repo_trade} == $bond ) {
+
+                        push @trades, $repo_trade;
+                        print FILE ", $repo_trade";
+
+                        if ( scalar( @trades ) >= $MAX_TRADES_PER_BOOK ) {
+
+                            print FILE " );\n";
+                           
+                            $book_number++; 
+                            $id = get_next_id();
+                            $name = "REPO_".$ir_curve_name{$ir_curve}."_BK$book_number";
+                            @trades = [];
+
+                            print FILE "insert into object values ( $id, 7, 4, \"$name\", $log_level, 1 );\n";
+                            print FILE "insert into repo_book values ( $id );";
+                            print FILE "insert into repo_book_repo_trades values ( $id";
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        while ( scalar( @trades ) < $MAX_TRADES_PER_BOOK ) {
+
+            push @trades, 0;
+            print FILE ", 0";
+        }
+        
+        print FILE " );\n";
+    }
+
+    print FILE "COMMIT;\n";
+
+    close FILE;
+}
 sub get_next_id() {
 
     return $current_id++;
