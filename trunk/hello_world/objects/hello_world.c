57451aedc5021ec7a2847071490afb93badfae14
schevans <schevans@46c0e2a4-fd5a-4016-b34e-fd37c1b5dcbf>
2012-01-31 19:37:43 +0000

    Changed 'int id' => 'long id' in calculate function signature (t4v4.0 upgrade).

--- trunk/hello_world/objects/hello_world.c
+++ trunk/hello_world/objects/hello_world.c
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-int calculate_hello_world( obj_loc_t obj_loc, int id )
+int calculate_hello_world( obj_loc_t obj_loc, long id )
 {
     cout << object_name(hello_world_hello) << " " << object_name(hello_world_world) << "!" << endl;
 
--- trunk/hello_world/objects/world.c
+++ trunk/hello_world/objects/world.c
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-int calculate_world( obj_loc_t obj_loc, int id )
+int calculate_world( obj_loc_t obj_loc, long id )
 {
     // Does nothing.
 
